9c5fd1dcc5039ec7b5c253c8db7e300b6bfc5db2
TyroneMF <TyroneMF@hotmail.com>
2020-02-20 04:33:12 +0000

    PMAG/EMAG Variants.
    - Adds PMAG/EMAG Variants for ACE ammunition.
    - Vanilla magazine renaming.
    - Changed 000 Buckshot to 00 Magnum

--- addons/ammunition/config.cpp
+++ addons/ammunition/config.cpp
@@ -6,12 +6,20 @@ class CfgPatches {
         units[] = {};
         weapons[] = {};
         requiredVersion = REQUIRED_VERSION;
-        requiredAddons[] = {"tacgt_main"};
+        requiredAddons[] = {"tacgt_main", "CUP_Weapons_Ammunition"};
         magazines[] = {
             "tacgt_8Rnd_P_000",
             "tacgt_6Rnd_P_000",
             "tacgt_8Rnd_S_AP20",
-            "tacgt_6Rnd_S_AP20"
+            "tacgt_6Rnd_S_AP20",
+            "tacgt_30Rnd_556x45_M855_PMAG",
+            "tacgt_30Rnd_556x45_M995_PMAG",
+            "tacgt_30Rnd_556x45_MK262_PMAG",
+            "tacgt_30Rnd_556x45_MK318_PMAG",
+            "tacgt_30Rnd_556x45_M855_EMAG",
+            "tacgt_30Rnd_556x45_M995_EMAG",
+            "tacgt_30Rnd_556x45_MK262_EMAG",
+            "tacgt_30Rnd_556x45_MK318_EMAG"
         };
         ammo[] = {
             "tacgt_S_12G_AP20",
