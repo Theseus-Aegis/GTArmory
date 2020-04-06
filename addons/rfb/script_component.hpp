d6ec4a4b0f660bf48a74e5fb82baa700ec459c91
TyroneMF <TyroneMF@hotmail.com>
2020-04-06 16:02:44 +0100

    Convert SDAR To RFB
    - title

--- addons/rfb/script_component.hpp
+++ addons/rfb/script_component.hpp
@@ -1,4 +1,4 @@
-#define COMPONENT sdar
-#define COMPONENT_BEAUTIFIED SDAR
+#define COMPONENT rfb
+#define COMPONENT_BEAUTIFIED RFB
 #include "\x\tacgt\addons\main\script_mod.hpp"
 #include "\x\tacgt\addons\main\script_macros.hpp"
