ec352d872b2a53eadebbbf6728f4d8a4bb0fe271
TyroneMF <TyroneMF@hotmail.com>
2020-04-22 06:06:26 +0100

    Remove 6.8 SPC MX
    - Original removed parts of the textures show from certain angles, seems to be a problem with the MX that i can't fix.

--- addons/mx/CfgAmmo.hpp
+++
@@ -1,9 +0,0 @@
-class CfgAmmo {
-    class B_65x39_Caseless_green;
-    
-    class CLASS(68x43_SPC): B_65x39_Caseless_green {
-        hit = 11.6;
-        caliber = 1.1;
-        typicalSpeed = 802;
-    };
-};
--- addons/mx/script_component.hpp
+++
@@ -1,4 +0,0 @@
-#define COMPONENT mx
-#define COMPONENT_BEAUTIFIED MX
-#include "\x\tacgt\addons\main\script_mod.hpp"
-#include "\x\tacgt\addons\main\script_macros.hpp"
