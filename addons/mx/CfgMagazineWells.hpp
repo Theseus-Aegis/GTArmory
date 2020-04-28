1c54d5e19a5f4abdd24ea5bdd53f57e63b81bfa7
TyroneMF <TyroneMF@hotmail.com>
2020-04-28 04:21:40 +0100

    Remove 6.8 SPC MX (#26)

--- addons/mx/CfgMagazineWells.hpp
+++
@@ -1,8 +0,0 @@
-class CfgMagazineWells {
-    class CLASS(68x43_MX) {
-        ADDON[] = {
-            QCLASS(30Rnd_68x43_MX_Sand),
-            QCLASS(30Rnd_68x43_MX_Black)
-        };
-    };
-};
