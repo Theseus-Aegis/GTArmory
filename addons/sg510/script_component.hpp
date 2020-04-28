651f31af6c3ac0bc95133cf307fcb76945a9e879
TyroneMF <TyroneMF@hotmail.com>
2020-04-28 04:21:52 +0100

    Remove SIG SG510 Sound Component (#28)

--- addons/sg510/script_component.hpp
+++
@@ -1,7 +0,0 @@
-#define COMPONENT sg510
-#define COMPONENT_BEAUTIFIED SG510
-#include "\x\tacgt\addons\main\script_mod.hpp"
-#include "\x\tacgt\addons\main\script_macros.hpp"
-
-#define SG510_SOUND_SET "tacgt_sg510_Shot_SoundSet", "tacgt_sg510_Tail_SoundSet"
-#define SG510S_SOUND_SET "tacgt_sg510_silencerShot_SoundSet", "tacgt_sg510_silencerTail_SoundSet"
