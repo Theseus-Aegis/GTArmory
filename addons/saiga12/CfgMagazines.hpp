70c12e033315f630818955710b03f99e50c55daf
TyroneMF <TyroneMF@hotmail.com>
2020-05-03 00:28:24 +0100

    Update Saiga12 Magazine Counts
    - Updated Saiga12 magazine counts to match classname.
    - 5Rnd to 8Rnd
    - 12Rnd to 20Rnd

---
+++ addons/saiga12/CfgMagazines.hpp
@@ -0,0 +1,17 @@
+class CfgMagazines {
+    class CA_Magazine;
+    // 8Rnd
+    class CUP_8Rnd_B_Saiga12_74Pellets_M: CA_Magazine {
+        count = 8;
+    };
+    class CUP_8Rnd_B_Saiga12_74Slug_M: CA_Magazine {
+        count = 8;
+    };
+    // 20Rnd
+    class CUP_20Rnd_B_Saiga12_74Pellets_M: CUP_8Rnd_B_Saiga12_74Pellets_M {
+        count = 20;
+    };
+    class CUP_20Rnd_B_Saiga12_74Slug_M: CUP_8Rnd_B_Saiga12_74Slug_M {
+        count = 20;
+    };
+};
