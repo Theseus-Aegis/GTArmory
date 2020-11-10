23db4aab9400f2e35645db0002c51beb23685c53
TyroneMF <TyroneMF@hotmail.com>
2020-11-10 13:17:50 +0000

    Add TRG Component (#45)

---
+++ addons/trg/config.cpp
@@ -0,0 +1,22 @@
+#include "script_component.hpp"
+
+class CfgPatches {
+    class ADDON {
+        name = COMPONENT_NAME;
+        units[] = {};
+        weapons[] = {
+            QCLASS(TAR21_Black),
+            QCLASS(GTAR21_EGLM_Black),
+            QCLASS(CTAR21_Black)
+        };
+        magazines[] = {};
+        requiredVersion = REQUIRED_VERSION;
+        requiredAddons[] = {"tacgt_main"};
+        author = ECSTRING(main,Author);
+        authors[] = {"TyroneMF"};
+        url = ECSTRING(main,URL);
+        VERSION_CONFIG;
+    };
+};
+
+#include "CfgWeapons.hpp"
