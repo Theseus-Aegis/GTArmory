9193bd11fe9c6a4889d28c73623c9f6c418b57ea
TyroneMF <TyroneMF@hotmail.com>
2021-01-24 22:11:46 +0000

    Flashlights - Add 2 Pistol IR/Laser/Light combos (#56)

--- addons/flashlight/config.cpp
+++ addons/flashlight/config.cpp
@@ -44,7 +44,12 @@ class CfgPatches {
             QCLASS(Surefire_Flashlight_Tan),
             QCLASS(Surefire_Low_Flashlight_Tan),
             QCLASS(UTG_Flashlight),
-            QCLASS(UTG_Low_Flashlight)
+            QCLASS(UTG_Low_Flashlight),
+            QCLASS(CZ_M3X_Laser),
+            QCLASS(CZ_M3X_Light),
+            QCLASS(CZ_M3X_Vis_Laser),
+            QCLASS(MLPLS_Laser),
+            QCLASS(MLPLS_Vis_Laser)
         };
         magazines[] = {};
         requiredVersion = REQUIRED_VERSION;
@@ -59,46 +64,56 @@ class CfgPatches {
 class asdg_SlotInfo;
 class asdg_FrontSideRail: asdg_SlotInfo {
     class compatibleItems {
-    tacgt_Surefire_Low_Flashlight_Tan = 1;
-    tacgt_ANPEQ_15_Light_Black = 1;
-    tacgt_ANPEQ_15_Low_Light_Black = 1;
-    tacgt_ANPEQ_15_Laser_Black = 1;
-    tacgt_ANPEQ_15_Vis_Laser_Black = 1;
-    tacgt_ANPEQ_15_Light_OD = 1;
-    tacgt_ANPEQ_15_Low_Light_OD = 1;
-    tacgt_ANPEQ_15_Laser_OD = 1;
-    tacgt_ANPEQ_15_Vis_Laser_OD = 1;
-    tacgt_ANPEQ_15_Light_Tan = 1;
-    tacgt_ANPEQ_15_Low_Light_Tan = 1;
-    tacgt_ANPEQ_15_Laser_Tan = 1;
-    tacgt_ANPEQ_15_Vis_Laser_Tan = 1;
-    tacgt_ANPEQ_15_Top_Laser_Black = 1;
-    tacgt_ANPEQ_15_Top_Light_Black = 1;
-    tacgt_ANPEQ_15_Top_Low_Light_Black = 1;
-    tacgt_ANPEQ_15_Top_Vis_Laser_Black = 1;
-    tacgt_ANPEQ_15_Top_Light_Tan = 1;
-    tacgt_ANPEQ_15_Top_Low_Light_Tan = 1;
-    tacgt_ANPEQ_15_Top_Laser_Tan = 1;
-    tacgt_ANPEQ_15_Top_Vis_Laser_Tan = 1;
-    tacgt_ANPEQ_15_Top_Light_OD = 1;
-    tacgt_ANPEQ_15_Top_Low_Light_OD = 1;
-    tacgt_ANPEQ_15_Top_Laser_OD = 1;
-    tacgt_ANPEQ_15_Top_Vis_Laser_OD = 1;
-    tacgt_LLM01_Laser_Black = 1;
-    tacgt_LLM01_Light_Black = 1;
-    tacgt_LLM01_Low_Light_Black = 1;
-    tacgt_LLM01_Vis_Laser_Black = 1;
-    tacgt_LLM_Laser_Black = 1;
-    tacgt_LLM_Light_Black = 1;
-    tacgt_LLM_Low_Light_Black = 1;
-    tacgt_LLM_Vis_Laser_Black = 1;
-    tacgt_Surefire_Flashlight_Black = 1;
-    tacgt_Surefire_Low_Flashlight_Black = 1;
-    tacgt_Surefire_Flashlight_OD = 1;
-    tacgt_Surefire_Low_Flashlight_OD = 1;
-    tacgt_Surefire_Flashlight_Tan = 1;
-    tacgt_UTG_Flashlight = 1;
-    tacgt_UTG_Low_Flashlight = 1;
+        tacgt_Surefire_Low_Flashlight_Tan = 1;
+        tacgt_ANPEQ_15_Light_Black = 1;
+        tacgt_ANPEQ_15_Low_Light_Black = 1;
+        tacgt_ANPEQ_15_Laser_Black = 1;
+        tacgt_ANPEQ_15_Vis_Laser_Black = 1;
+        tacgt_ANPEQ_15_Light_OD = 1;
+        tacgt_ANPEQ_15_Low_Light_OD = 1;
+        tacgt_ANPEQ_15_Laser_OD = 1;
+        tacgt_ANPEQ_15_Vis_Laser_OD = 1;
+        tacgt_ANPEQ_15_Light_Tan = 1;
+        tacgt_ANPEQ_15_Low_Light_Tan = 1;
+        tacgt_ANPEQ_15_Laser_Tan = 1;
+        tacgt_ANPEQ_15_Vis_Laser_Tan = 1;
+        tacgt_ANPEQ_15_Top_Laser_Black = 1;
+        tacgt_ANPEQ_15_Top_Light_Black = 1;
+        tacgt_ANPEQ_15_Top_Low_Light_Black = 1;
+        tacgt_ANPEQ_15_Top_Vis_Laser_Black = 1;
+        tacgt_ANPEQ_15_Top_Light_Tan = 1;
+        tacgt_ANPEQ_15_Top_Low_Light_Tan = 1;
+        tacgt_ANPEQ_15_Top_Laser_Tan = 1;
+        tacgt_ANPEQ_15_Top_Vis_Laser_Tan = 1;
+        tacgt_ANPEQ_15_Top_Light_OD = 1;
+        tacgt_ANPEQ_15_Top_Low_Light_OD = 1;
+        tacgt_ANPEQ_15_Top_Laser_OD = 1;
+        tacgt_ANPEQ_15_Top_Vis_Laser_OD = 1;
+        tacgt_LLM01_Laser_Black = 1;
+        tacgt_LLM01_Light_Black = 1;
+        tacgt_LLM01_Low_Light_Black = 1;
+        tacgt_LLM01_Vis_Laser_Black = 1;
+        tacgt_LLM_Laser_Black = 1;
+        tacgt_LLM_Light_Black = 1;
+        tacgt_LLM_Low_Light_Black = 1;
+        tacgt_LLM_Vis_Laser_Black = 1;
+        tacgt_Surefire_Flashlight_Black = 1;
+        tacgt_Surefire_Low_Flashlight_Black = 1;
+        tacgt_Surefire_Flashlight_OD = 1;
+        tacgt_Surefire_Low_Flashlight_OD = 1;
+        tacgt_Surefire_Flashlight_Tan = 1;
+        tacgt_UTG_Flashlight = 1;
+        tacgt_UTG_Low_Flashlight = 1;
+    };
+};
+
+class asdg_PistolUnderRail: asdg_SlotInfo {
+    class compatibleItems {
+        tacgt_CZ_M3X_Laser = 1;
+        tacgt_CZ_M3X_Light = 1;
+        tacgt_CZ_M3X_Vis_Laser = 1;
+        tacgt_MLPLS_Laser = 1;
+        tacgt_MLPLS_Vis_Laser = 1;
     };
 };
 
