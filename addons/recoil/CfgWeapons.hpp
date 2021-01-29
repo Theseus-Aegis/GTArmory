56856996c4d51706a518807ef9a599c39d4fa374
TyroneMF <TyroneMF@hotmail.com>
2021-01-29 14:05:31 +0000

    Add Recoil Component (#60)

---
+++ addons/recoil/CfgWeapons.hpp
@@ -0,0 +1,8 @@
+class CfgWeapons {
+    class Rifle_Base_F;
+
+    // CUP HK416, Uses recoil from SPAR-16 from Apex.
+    class CUP_arifle_HK416_145_Base: Rifle_Base_F {
+        recoil = "recoil_spar";
+    };
+};
