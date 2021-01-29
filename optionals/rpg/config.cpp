89ea5a221af121b094d7b2a2267ad6562e484fe2
TyroneMF <TyroneMF@hotmail.com>
2021-01-29 14:06:30 +0000

    Add Optional RPG Component (#59)

---
+++ optionals/rpg/config.cpp
@@ -0,0 +1,21 @@
+#include "script_component.hpp"
+
+class CfgPatches {
+    class ADDON {
+        name = COMPONENT_NAME;
+        units[] = {};
+        weapons[] = {};
+        magazines[] = {};
+        requiredVersion = REQUIRED_VERSION;
+        requiredAddons[] = {"tacgt_main", "CUP_Weapons_RPG7", "CUP_Weapons_Ammunition"};
+        author = ECSTRING(main,Author);
+        authors[] = {"TyroneMF"};
+        url = ECSTRING(main,URL);
+        VERSION_CONFIG;
+    };
+};
+
+class Mode_SemiAuto;
+
+#include "CfgAmmo.hpp"
+#include "CfgWeapons.hpp"
