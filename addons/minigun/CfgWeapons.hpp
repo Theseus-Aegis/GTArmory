32384e2d83d25e682839bad82723cdac97b286f1
TyroneMF <TyroneMF@hotmail.com>
2021-05-18 20:28:16 +0100

    Add Minigun Component (#70)

---
+++ addons/minigun/CfgWeapons.hpp
@@ -0,0 +1,12 @@
+class CfgWeapons {
+    class LMG_RCWS;
+    class MGun;
+
+    class LMG_Minigun: LMG_RCWS {
+        class manual: MGun {
+            class StandardSound {
+                soundSetShot[] = {"CUP_M134_Shot_SoundSet", "CUP_mmg1_Tail_SoundSet"};
+            };
+        };
+    };
+};
