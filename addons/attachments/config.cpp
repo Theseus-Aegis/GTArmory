3b7fe04488eab0cbee5308b26adb972d4b0dee81
TyroneMF <TyroneMF@hotmail.com>
2021-05-20 23:20:31 +0100

    initial push

---
+++ addons/attachments/config.cpp
@@ -0,0 +1,19 @@
+#include "script_component.hpp"
+
+class CfgPatches {
+    class ADDON {
+        name = COMPONENT_NAME;
+        units[] = {};
+        weapons[] = {};
+        magazines[] = {};
+        requiredVersion = REQUIRED_VERSION;
+        requiredAddons[] = {"tacgt_main", "tacgt_flashlight", "CUP_Weapons_West_Attachments"};
+        author = ECSTRING(main,Authors);
+        authors[] = {"Tyrone"};
+        url = ECSTRING(main,URL);
+        VERSION_CONFIG;
+    };
+};
+
+#include "PointerSlot.hpp"
+#include "CowsSlot_Rail.hpp"
---
+++ addons/attachments/script_component.hpp
@@ -0,0 +1,4 @@
+#define COMPONENT attachments
+#define COMPONENT_BEAUTIFIED Attachments
+#include "\x\tacgt\addons\main\script_mod.hpp"
+#include "\x\tacgt\addons\main\script_macros.hpp"
