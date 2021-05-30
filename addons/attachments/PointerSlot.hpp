7458670d08e723c9ac360f19426ce63346d01627
TyroneMF <TyroneMF@hotmail.com>
2021-05-30 08:49:05 +0100

    Add Attachments Component (#73)

---
+++ addons/attachments/PointerSlot.hpp
@@ -0,0 +1,88 @@
+class PointerSlot;
+class PointerSlot_Rail: PointerSlot {
+    class compatibleItems {
+        // CUP Stuff
+        CUP_acc_ANPEQ_15 = 1;
+        CUP_acc_ANPEQ_15_V = 1;
+        CUP_acc_ANPEQ_15_Black = 1;
+        CUP_acc_ANPEQ_15_Black_V = 1;
+        CUP_acc_ANPEQ_15_OD = 1;
+        CUP_acc_ANPEQ_15_OD_V = 1;
+
+        CUP_acc_ANPEQ_2 = 1;
+        CUP_acc_ANPEQ_2_grey = 1;
+        CUP_acc_ANPEQ_2_camo = 1;
+        CUP_acc_ANPEQ_2_desert = 1;
+
+        CUP_acc_Flashlight = 1;
+        CUP_acc_Flashlight_wdl = 1;
+        CUP_acc_Flashlight_desert = 1;
+
+        CUP_acc_XM8_light_module = 1;
+
+        CUP_acc_LLM = 1;
+        CUP_acc_LLM_Flashlight = 1;
+        CUP_acc_LLM_black = 1;
+        CUP_acc_LLM_black_Flashlight = 1;
+        CUP_acc_LLM_od = 1;
+        CUP_acc_LLM_od_Flashlight = 1;
+
+        CUP_acc_ANPEQ_2_Flashlight_Black_L = 1;
+        CUP_acc_ANPEQ_2_Flashlight_Black_F = 1;
+        CUP_acc_ANPEQ_2_Flashlight_Coyote_L = 1;
+        CUP_acc_ANPEQ_2_Flashlight_Coyote_F = 1;
+        CUP_acc_ANPEQ_2_Flashlight_OD_L = 1;
+        CUP_acc_ANPEQ_2_Flashlight_OD_F = 1;
+
+        CUP_acc_ANPEQ_15_Flashlight_Tan_L = 1;
+        CUP_acc_ANPEQ_15_Flashlight_Tan_F = 1;
+        CUP_acc_ANPEQ_15_Flashlight_Tan_V = 1;
+        CUP_acc_ANPEQ_15_Flashlight_OD_L = 1;
+        CUP_acc_ANPEQ_15_Flashlight_OD_F = 1;
+        CUP_acc_ANPEQ_15_Flashlight_OD_V = 1;
+        CUP_acc_ANPEQ_15_Flashlight_Black_L = 1;
+        CUP_acc_ANPEQ_15_Flashlight_Black_F = 1;
+        CUP_acc_ANPEQ_15_Flashlight_Black_V = 1;
+
+        CUP_acc_LLM01_F = 1;
+        CUP_acc_LLM01_L = 1;
+        CUP_acc_LLM01_desert_F = 1;
+        CUP_acc_LLM01_desert_L = 1;
+        CUP_acc_LLM01_hex_F = 1;
+        CUP_acc_LLM01_hex_L = 1;
+        CUP_acc_LLM01_coyote_F = 1;
+        CUP_acc_LLM01_coyote_L = 1;
+        CUP_acc_LLM01_od_F = 1;
+        CUP_acc_LLM01_od_L = 1;
+
+        // GT Armory
+        tacgt_Surefire_Low_Flashlight_Tan = 1;
+        tacgt_ANPEQ_15_Light_Black = 1;
+        tacgt_ANPEQ_15_Low_Light_Black = 1;
+        tacgt_ANPEQ_15_Laser_Black = 1;
+        tacgt_ANPEQ_15_Vis_Laser_Black = 1;
+        tacgt_ANPEQ_15_Light_OD = 1;
+        tacgt_ANPEQ_15_Low_Light_OD = 1;
+        tacgt_ANPEQ_15_Laser_OD = 1;
+        tacgt_ANPEQ_15_Vis_Laser_OD = 1;
+        tacgt_ANPEQ_15_Light_Tan = 1;
+        tacgt_ANPEQ_15_Low_Light_Tan = 1;
+        tacgt_ANPEQ_15_Laser_Tan = 1;
+        tacgt_ANPEQ_15_Vis_Laser_Tan = 1;
+        tacgt_LLM01_Laser_Black = 1;
+        tacgt_LLM01_Light_Black = 1;
+        tacgt_LLM01_Low_Light_Black = 1;
+        tacgt_LLM01_Vis_Laser_Black = 1;
+        tacgt_LLM_Laser_Black = 1;
+        tacgt_LLM_Light_Black = 1;
+        tacgt_LLM_Low_Light_Black = 1;
+        tacgt_LLM_Vis_Laser_Black = 1;
+        tacgt_Surefire_Flashlight_Black = 1;
+        tacgt_Surefire_Low_Flashlight_Black = 1;
+        tacgt_Surefire_Flashlight_OD = 1;
+        tacgt_Surefire_Low_Flashlight_OD = 1;
+        tacgt_Surefire_Flashlight_Tan = 1;
+        tacgt_UTG_Flashlight = 1;
+        tacgt_UTG_Low_Flashlight = 1;
+    };
+};
