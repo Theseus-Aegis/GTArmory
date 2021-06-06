0034e04bcf6679390e75800679dbed9e2401d6a2
TyroneMF <TyroneMF@hotmail.com>
2021-06-06 22:48:41 +0100

    Extend flare lifetime to 3 minutes

--- addons/flares/CfgAmmo.hpp
+++ addons/flares/CfgAmmo.hpp
@@ -1,14 +1,33 @@
 class CfgAmmo {
+
+    // BI
+    class FlareBase;
+    class F_40mm_White: FlareBase {
+        timeToLive = 180;
+    };
+    class F_40mm_Yellow: F_40mm_White {};
+    class F_40mm_Red: F_40mm_White {};
+    class F_40mm_Green: F_40mm_White {};
+
+    // Cluster
     class CUP_F_40mm_Star_White;
     class CUP_F_40mm_StarCluster_White: CUP_F_40mm_Star_White {
-        timeToLive = 60;
+        timeToLive = 180;
     };
 
     class CUP_F_40mm_StarCluster_Red: CUP_F_40mm_Star_White {
-        timeToLive = 60;
+        timeToLive = 180;
     };
 
     class CUP_F_40mm_StarCluster_Green: CUP_F_40mm_Star_White {
-        timeToLive = 60;
+        timeToLive = 180;
     };
+
+    // Parachute
+    class CUP_F_40mm_Star_White: F_40mm_White {
+        timeToLive = 180;
+    };
+
+    class CUP_F_40mm_Star_Green: CUP_F_40mm_Star_White {};
+    class CUP_F_40mm_Star_Red: CUP_F_40mm_Star_White {};
 };
