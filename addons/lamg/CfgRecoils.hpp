075bb09040f4f20116b0c2b704d40f19cba760eb
TyroneMF <TyroneMF@hotmail.com>
2021-08-01 21:29:10 +0100

    New Sig SG recoil and LAMG recoil tweak

--- addons/lamg/CfgRecoils.hpp
+++ addons/lamg/CfgRecoils.hpp
@@ -1,7 +1,7 @@
 class CfgRecoils {
     class recoil_default;
     class recoil_mk200: recoil_default {
-        muzzleOuter[] = {0.3, 0.7, 0.5, 0.2};
+        muzzleOuter[] = {0.3, 0.7, 0.4, 0.2};
         kickBack[] = {0.02, 0.04};
         temporary = 0.005;
     };
