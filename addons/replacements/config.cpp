015990f44e2063b7602c5f748838c6e0c319401c
TyroneMF <TyroneMF@hotmail.com>
2021-08-13 12:13:55 +0100

    Add Replacements Component (#81)
    * Initial Push
    
    * more stuff
    
    * Working IFAK
    
    * Remove unnecessary inheritance
    
    * Create base class, add grenade case, separate item replacement into files
    
    * Smoke Grenade case, M4 weapons case & Lootboxes
    
    * inherit from base class in mods & change config entry name.
    
    * Update base class
    
    * Smoke case UI image, add Battle rifles/shotguns to rifle list.
    
    * UI Images for boxes
    
    * M4 Case UI image
    
    * Smoke/Grenade case image tweak
    
    * Move functionality to TAC Mods
    
    * Remove script stuff
    
    * Update script_macros.hpp
    
    * Fix indentation, hide helper box and remove editor categories.

---
+++ addons/replacements/config.cpp
@@ -0,0 +1,21 @@
+#include "script_component.hpp"
+
+class CfgPatches {
+    class ADDON {
+        name = COMPONENT_NAME;
+        units[] = {
+            QCLASS(Replacement_Box)
+        };
+        weapons[] = {};
+        magazines[] = {};
+        requiredVersion = REQUIRED_VERSION;
+        requiredAddons[] = {"tacgt_main", "tac_supplies"};
+        author = ECSTRING(main,Authors);
+        authors[] = {"Tyrone"};
+        url = ECSTRING(main,URL);
+        VERSION_CONFIG;
+    };
+};
+
+#include "CfgVehicles.hpp"
+#include "CfgWeapons.hpp"
