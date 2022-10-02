2bbcc7b1154d10c342c878d2e539f4f4b2d69a08
Mike-MF <TyroneMF@hotmail.com>
2022-10-02 18:09:58 +0100

    Add Spare Barrel Component (#112)
    Co-authored-by: JoramD <j.davids@hotmail.nl>

---
+++ addons/sparebarrel/CfgWeapons.hpp
@@ -0,0 +1,9 @@
+class CfgWeapons {
+    class ACE_ItemCore;
+    class CBA_MiscItem_ItemInfo;
+    class ACE_SpareBarrel_Item: ACE_ItemCore {
+        class ItemInfo: CBA_MiscItem_ItemInfo {
+            mass = 30; // Default: 60
+        };
+    };
+};
---
+++ addons/sparebarrel/config.cpp
@@ -0,0 +1,18 @@
+#include "script_component.hpp"
+
+class CfgPatches {
+    class ADDON {
+        name = COMPONENT_NAME;
+        units[] = {};
+        weapons[] = {};
+        magazines[] = {};
+        requiredVersion = REQUIRED_VERSION;
+        requiredAddons[] = {"tacgt_main", "ace_overheating"};
+        author = ECSTRING(main,Authors);
+        authors[] = {"Mike"};
+        url = ECSTRING(main,URL);
+        VERSION_CONFIG;
+    };
+};
+
+#include "CfgWeapons.hpp"
