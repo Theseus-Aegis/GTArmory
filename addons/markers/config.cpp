867d3f3ed7ce5e3701106184ab73ba9b51c3e26a
Mike-MF <TyroneMF@hotmail.com>
2022-10-08 18:17:00 +0100

    Add Markers Component (#113)

---
+++ addons/markers/config.cpp
@@ -0,0 +1,19 @@
+#include "script_component.hpp"
+
+class CfgPatches {
+    class ADDON {
+        name = COMPONENT_NAME;
+        units[] = {};
+        weapons[] = {};
+        magazines[] = {};
+        requiredVersion = REQUIRED_VERSION;
+        requiredAddons[] = {"tacgt_main", "ace_map"};
+        author = ECSTRING(main,Authors);
+        authors[] = {"Mike"};
+        url = ECSTRING(main,URL);
+        VERSION_CONFIG;
+    };
+};
+
+#include "CfgMarkerClasses.hpp"
+#include "CfgMarkers.hpp"
