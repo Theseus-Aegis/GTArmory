0cd3c89b105876291fc1c7873270dac8c171eb0b
Mike-MF <TyroneMF@hotmail.com>
2022-10-20 15:32:01 +0100

    Update CfgVehicles.hpp

--- addons/entrenching/CfgVehicles.hpp
+++ addons/entrenching/CfgVehicles.hpp
@@ -9,7 +9,7 @@ class CfgVehicles {
     class CUP_O_RUS_Patrol_bag_Summer_Shovel: Bag_Base {
         ace_trenches_entrenchingTool = 1;
     };
-    class CUP_O_RUS_Patrol_bag_Desert_Shovel: Bag_Base {
+    class CUP_O_RUS_Patrol_bag_Green_Shovel: Bag_Base {
         ace_trenches_entrenchingTool = 1;
     };
 };
