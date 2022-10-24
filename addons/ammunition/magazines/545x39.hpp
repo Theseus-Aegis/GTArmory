04bc2ec20e7aa7cd83b62c0435efcde1ee1fb552
Mike-MF <TyroneMF@hotmail.com>
2022-10-24 23:02:18 +0100

    Ammunition - Add magazine tags (#128)

--- addons/ammunition/magazines/545x39.hpp
+++ addons/ammunition/magazines/545x39.hpp
@@ -3,25 +3,29 @@ class CLASS(30Rnd_545x39_Ball): 30Rnd_545x39_Mag_F {
     MACRO_SCOPE
     MACRO_AK12_MAGAZINE_TAN
     ammo = QCLASS(545x39_Ball);
-    descriptionShort = "5.45x39mm Ball Reload Tracer";
     displayName = "5.45mm 30Rnd Tan (Ball)";
+    displayNameShort = "Ball";
+    descriptionShort = "5.45x39mm Ball Reload Tracer";
     lastRoundsTracer = 4;
     mass = 10;
 };
 class CLASS(30Rnd_545x39_Ball_Tracer): CLASS(30Rnd_545x39_Ball) {
-    descriptionShort = "5.45x39mm Ball Tracer";
     displayName = "5.45mm 30Rnd Tan [T] (Ball)";
+    displayNameShort = "Ball Tracer";
+    descriptionShort = "5.45x39mm Ball Tracer";
     tracersEvery = 1;
 };
 class CLASS(30Rnd_545x39_EPR): CLASS(30Rnd_545x39_Ball) {
     ammo = QCLASS(545x39_EPR);
-    descriptionShort = "5.45x39mm EPR Reload Tracer";
     displayName = "5.45mm 30Rnd Tan (EPR)";
+    displayNameShort = "EPR";
+    descriptionShort = "5.45x39mm EPR Reload Tracer";
 };
 class CLASS(30Rnd_545x39_AP): CLASS(30Rnd_545x39_EPR) {
     ammo = QCLASS(545x39_AP);
-    descriptionShort = "5.45x39mm AP Reload Tracer";
     displayName = "5.45mm 30Rnd Tan (AP)";
+    displayNameShort = "AP";
+    descriptionShort = "5.45x39mm AP Reload Tracer";
 };
 
 // 5.45x39mm Black
@@ -47,43 +51,50 @@ class CLASS(30Rnd_545x39_AP_Black): CLASS(30Rnd_545x39_AP) {
 class CLASS(60Rnd_545x39_Ball_Quadstack): CUP_60Rnd_545x39_AK74M_M {
     MACRO_SCOPE
     ammo = QCLASS(545x39_Ball);
-    descriptionShort = "5.45x39mm Ball Tracer Every 4";
     displayName = "5.45mm 60Rnd Quadstack [TE4] (Ball)";
+    displayNameShort = "Ball TE4";
+    descriptionShort = "5.45x39mm Ball Tracer Every 4";
     tracersEvery = 4;
     mass = 20;
 };
 class CLASS(60Rnd_545x39_Ball_Tracer_Quadstack): CLASS(60Rnd_545x39_Ball_Quadstack) {
-    descriptionShort = "5.45x39mm Ball Tracer";
     displayName = "5.45mm 60Rnd Quadstack [T] (Ball)";
+    displayNameShort = "Ball Tracer";
+    descriptionShort = "5.45x39mm Ball Tracer";
     tracersEvery = 1;
 };
 class CLASS(60Rnd_545x39_EPR_Quadstack): CLASS(60Rnd_545x39_Ball_Quadstack) {
     ammo = QCLASS(545x39_EPR);
-    descriptionShort = "5.45x39mm EPR Tracer Every 4";
     displayName = "5.45mm 60Rnd Quadstack [TE4] (EPR)";
+    displayNameShort = "EPR TE4";
+    descriptionShort = "5.45x39mm EPR Tracer Every 4";
 };
 
 // 5.45x39mm Fort
 class CLASS(30Rnd_545x39_Ball_Fort): CUP_30Rnd_545x39_Fort224_M {
     MACRO_SCOPE
     ammo = QCLASS(545x39_Ball);
-    descriptionShort = "5.45x39mm Ball Reload Tracer";
     displayName = "5.45mm 30Rnd Fort (Ball)";
+    displayNameShort = "Ball";
+    descriptionShort = "5.45x39mm Ball Reload Tracer";
     lastRoundsTracer = 4;
     mass = 10;
 };
 class CLASS(30Rnd_545x39_Ball_Tracer_Fort): CLASS(30Rnd_545x39_Ball_Fort) {
-    descriptionShort = "5.45x39mm Ball Tracer";
     displayName = "5.45mm 30Rnd Fort [T] (Ball)";
+    displayNameShort = "Ball Tracer";
+    descriptionShort = "5.45x39mm Ball Tracer";
     tracersEvery = 1;
 };
 class CLASS(30Rnd_545x39_EPR_Fort): CLASS(30Rnd_545x39_Ball_Fort) {
     ammo = QCLASS(545x39_EPR);
-    descriptionShort = "5.45x39mm EPR Reload Tracer";
     displayName = "5.45mm 30Rnd Fort (EPR)";
+    displayNameShort = "EPR";
+    descriptionShort = "5.45x39mm EPR Reload Tracer";
 };
 class CLASS(30Rnd_545x39_AP_Fort): CLASS(30Rnd_545x39_EPR_Fort) {
     ammo = QCLASS(545x39_AP);
-    descriptionShort = "5.45x39mm AP Reload Tracer";
     displayName = "5.45mm 30Rnd Fort (AP)";
+    displayNameShort = "AP";
+    descriptionShort = "5.45x39mm AP Reload Tracer";
 };
--- addons/ammunition/magazines/68x43.hpp
+++ addons/ammunition/magazines/68x43.hpp
@@ -2,23 +2,27 @@
 class CLASS(30Rnd_68x43_Ball_ACR): CUP_30Rnd_680x43_Stanag {
     MACRO_SCOPE
     ammo = QCLASS(68x43_Ball);
-    descriptionShort = "6.8x43mm Ball Reload Tracer";
     displayName = "6.8mm 30Rnd ACR (Ball)";
+    displayNameShort = "Ball";
+    descriptionShort = "6.8x43mm Ball Reload Tracer";
     lastRoundsTracer = 4;
     mass = 10;
 };
 class CLASS(30Rnd_68x43_Ball_Tracer_ACR): CLASS(30Rnd_68x43_Ball_ACR) {
-    descriptionShort = "6.8x43mm Ball Tracer";
     displayName = "6.8mm 30Rnd ACR [T] (Ball)";
+    displayNameShort = "Ball Tracer";
+    descriptionShort = "6.8x43mm Ball Tracer";
     tracersEvery = 1;
 };
 class CLASS(30Rnd_68x43_EPR_ACR): CLASS(30Rnd_68x43_Ball_ACR) {
     ammo = QCLASS(68x43_EPR);
-    descriptionShort = "6.8x43mm EPR Reload Tracer";
     displayName = "6.8mm 30Rnd ACR (EPR)";
+    displayNameShort = "EPR";
+    descriptionShort = "6.8x43mm EPR Reload Tracer";
 };
 class CLASS(30Rnd_68x43_AP_ACR): CLASS(30Rnd_68x43_EPR_ACR) {
     ammo = QCLASS(68x43_AP);
-    descriptionShort = "6.8x43mm AP Reload Tracer";
     displayName = "6.8mm 30Rnd ACR (AP)";
+    displayNameShort = "AP";
+    descriptionShort = "6.8x43mm AP Reload Tracer";
 };
