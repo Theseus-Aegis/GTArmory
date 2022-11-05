bf17b6428e27ef55d3ca40c247ba4d26fc1dcc19
Mike-MF <TyroneMF@hotmail.com>
2022-11-05 22:12:17 +0000

    Add Arcadian Component

---
+++ addons/arcadian/CfgMagazines.hpp
@@ -0,0 +1,15 @@
+class CfgMagazines {
+    class 2000Rnd_65x39_Belt_Tracer_Yellow;
+    class CLASS(2000Rnd_65_EPR_Arcadian): 2000Rnd_65x39_Belt_Tracer_Yellow {
+        ammo = QCLASS(65x39_EPR);
+        displayName = "6.5mm 2000Rnd M134 [TE4] (EPR)";
+        tracersEvery = 4;
+    };
+
+    class 1000Rnd_65x39_Belt_Tracer_Yellow;
+    class CLASS(1000Rnd_65_EPR_Arcadian): 1000Rnd_65x39_Belt_Tracer_Yellow {
+        ammo = QCLASS(65x39_EPR);
+        displayName = "6.5mm 1000Rnd M134 [TE4] (EPR)";
+        tracersEvery = 4;
+    };
+};
