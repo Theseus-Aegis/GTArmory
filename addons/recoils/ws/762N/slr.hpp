43664bb724d4d6ba60573d1aee418dffd8d7a1f4
Mike-MF <TyroneMF@hotmail.com>
2023-01-13 13:55:42 +0000

    Add other SLR GL to recoils

--- addons/recoils/ws/762N/slr.hpp
+++ addons/recoils/ws/762N/slr.hpp
@@ -5,3 +5,7 @@ class arifle_SLR_lxWS: DMR_06_base_F {
 class arifle_SLR_GL_lxWS: arifle_SLR_lxWS {
     recoil = QCLASS(762N_GL_Long);
 };
+
+class arifle_SLR_V_GL_lxWS: arifle_SLR_V_lxWS {
+    recoil = QCLASS(762N_GL_Long);
+};
