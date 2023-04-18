e0874b75c2577522bfba6cc96ade894d6c880214
jonpas <jonpas33@gmail.com>
2023-04-18 18:35:22 +0200

    Fix typo

--- addons/headgear/script_component.hpp
+++ addons/headgear/script_component.hpp
@@ -1,4 +1,4 @@
 #define COMPONENT headgear
-#define COMPONENT_BEAUTIFIED Heeadgear
+#define COMPONENT_BEAUTIFIED Headgear
 #include "\x\tacgt\addons\main\script_mod.hpp"
 #include "\x\tacgt\addons\main\script_macros.hpp"
