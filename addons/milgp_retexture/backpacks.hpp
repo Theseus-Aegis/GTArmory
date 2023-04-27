ccc5895a33406827c796748e3b57a444f7367763
Joram Davids <j.davids@hotmail.nl>
2023-04-27 17:06:23 +0200

    Sort backpacks

--- addons/milgp_retexture/backpacks.hpp
+++ addons/milgp_retexture/backpacks.hpp
@@ -1,61 +1,4 @@
-// Grey
-class milgp_bp_hydration_rgr;
-class CLASS(hydration_gry): milgp_bp_hydration_rgr {
-    displayName = "MarCiras Back Panel Hydration (GRY)";
-    author = "GilleeDoo";
-    hiddenSelections[] = {"camo"};
-    hiddenSelectionsTextures[] = {QPATHTOF(data\v_gear_01_gry_co.paa)};
-};
-
-// Black
-class CLASS(hydration_blk): milgp_bp_hydration_rgr {
-    displayName = "MarCiras Back Panel Hydration (BLK)";
-    author = "GilleeDoo";
-    hiddenSelections[] = {"camo"};
-    hiddenSelectionsTextures[] = {QPATHTOF(data\v_gear_01_blk_co.paa)};
-};
-
-// Grey
-class milgp_bp_Tomahawk_rgr;
-class CLASS(tomahawk_gry): milgp_bp_Tomahawk_rgr {
-    displayName = "MarCiras Back Panel Tomahawk (GRY)";
-    author = "GilleeDoo";
-    hiddenSelections[] = {"camo","camo1"};
-    hiddenSelectionsTextures[] = {
-        QPATHTOF(data\v_gear_01_gry_co.paa),
-        QPATHTOF(data\b_cryedro_gry_co.paa)
-    };
-};
-
-// Black
-class CLASS(tomahawk_blk): milgp_bp_Tomahawk_rgr {
-    displayName = "MarCiras Back Panel Tomahawk (BLK)";
-    author = "GilleeDoo";
-    hiddenSelections[] = {"camo","camo1"};
-    hiddenSelectionsTextures[] = {
-        QPATHTOF(data\v_gear_01_blk_co.paa),
-        QPATHTOF(data\b_cryedro_blk_co.paa)
-    };
-};
-
-// Grey
-class milgp_bp_Pointman_rgr;
-class CLASS(pointman_gry): milgp_bp_Pointman_rgr {
-    displayName = "MarCiras Back Panel Pointman (GRY)";
-    author = "GilleeDoo";
-    hiddenSelections[] = {"camo"};
-    hiddenSelectionsTextures[] = {QPATHTOF(data\v_gear_01_gry_co.paa)};
-};
-
-// Black
-class CLASS(pointman_blk): milgp_bp_Pointman_rgr {
-    displayName = "MarCiras Back Panel Pointman (BLK)";
-    author = "GilleeDoo";
-    hiddenSelections[] = {"camo"};
-    hiddenSelectionsTextures[] = {QPATHTOF(data\v_gear_01_blk_co.paa)};
-};
-
-// Grey
+// Breacher Grey
 class milgp_bp_Breacher_rgr;
 class CLASS(breacher_gry): milgp_bp_Breacher_rgr {
     displayName = "MarCiras Back Panel Breacher (GRY)";
@@ -67,7 +10,7 @@ class CLASS(breacher_gry): milgp_bp_Breacher_rgr {
     };
 };
 
-// Black
+// Breacher Black
 class CLASS(breacher_blk): milgp_bp_Breacher_rgr {
     displayName = "MarCiras Back Panel Breacher (BLK)";
     author = "GilleeDoo";
@@ -78,7 +21,64 @@ class CLASS(breacher_blk): milgp_bp_Breacher_rgr {
     };
 };
 
-// Grey
+// Hydration Grey
+class milgp_bp_hydration_rgr;
+class CLASS(hydration_gry): milgp_bp_hydration_rgr {
+    displayName = "MarCiras Back Panel Hydration (GRY)";
+    author = "GilleeDoo";
+    hiddenSelections[] = {"camo"};
+    hiddenSelectionsTextures[] = {QPATHTOF(data\v_gear_01_gry_co.paa)};
+};
+
+// Hydration Black
+class CLASS(hydration_blk): milgp_bp_hydration_rgr {
+    displayName = "MarCiras Back Panel Hydration (BLK)";
+    author = "GilleeDoo";
+    hiddenSelections[] = {"camo"};
+    hiddenSelectionsTextures[] = {QPATHTOF(data\v_gear_01_blk_co.paa)};
+};
+
+// Pointman Grey
+class milgp_bp_Pointman_rgr;
+class CLASS(pointman_gry): milgp_bp_Pointman_rgr {
+    displayName = "MarCiras Back Panel Pointman (GRY)";
+    author = "GilleeDoo";
+    hiddenSelections[] = {"camo"};
+    hiddenSelectionsTextures[] = {QPATHTOF(data\v_gear_01_gry_co.paa)};
+};
+
+// Pointman Black
+class CLASS(pointman_blk): milgp_bp_Pointman_rgr {
+    displayName = "MarCiras Back Panel Pointman (BLK)";
+    author = "GilleeDoo";
+    hiddenSelections[] = {"camo"};
+    hiddenSelectionsTextures[] = {QPATHTOF(data\v_gear_01_blk_co.paa)};
+};
+
+// Tomahawk Grey
+class milgp_bp_Tomahawk_rgr;
+class CLASS(tomahawk_gry): milgp_bp_Tomahawk_rgr {
+    displayName = "MarCiras Back Panel Tomahawk (GRY)";
+    author = "GilleeDoo";
+    hiddenSelections[] = {"camo","camo1"};
+    hiddenSelectionsTextures[] = {
+        QPATHTOF(data\v_gear_01_gry_co.paa),
+        QPATHTOF(data\b_cryedro_gry_co.paa)
+    };
+};
+
+// Tomahawk Black
+class CLASS(tomahawk_blk): milgp_bp_Tomahawk_rgr {
+    displayName = "MarCiras Back Panel Tomahawk (BLK)";
+    author = "GilleeDoo";
+    hiddenSelections[] = {"camo","camo1"};
+    hiddenSelectionsTextures[] = {
+        QPATHTOF(data\v_gear_01_blk_co.paa),
+        QPATHTOF(data\b_cryedro_blk_co.paa)
+    };
+};
+
+// Patrol 01 Grey
 class milgp_b_patrol_01_rgr;
 class CLASS(patrol_01_gry): milgp_b_patrol_01_rgr {
     displayName = "Patrol 01 (GRY)";
@@ -90,7 +90,7 @@ class CLASS(patrol_01_gry): milgp_b_patrol_01_rgr {
     };
 };
 
-// Black
+// Patrol 01 Black
 class CLASS(patrol_01_blk): milgp_b_patrol_01_rgr {
     displayName = "Patrol 01 (BLK)";
     author = "GilleeDoo";
