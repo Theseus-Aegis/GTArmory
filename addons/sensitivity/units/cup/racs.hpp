558002291520201043b26edded58bb2a77af3098
Mike-MF <TyroneMF@hotmail.com>
2023-06-16 02:16:16 +0100

    CUP Removal

--- addons/sensitivity/units/cup/racs.hpp
+++
@@ -1,3 +0,0 @@
-class CUP_Creatures_Military_RACS_Soldier_Base: SoldierWB {
-    MACRO_SENSITIVITY
-};
--- addons/sensitivity/units/cup/usarmy.hpp
+++
@@ -1,3 +0,0 @@
-class CUP_Creatures_Military_USA_Soldier_Base: SoldierWB {
-    MACRO_SENSITIVITY
-};
