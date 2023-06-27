c51fcd84dc97ae9e683ccfa21966066ff6cb5913
Mike-MF <TyroneMF@hotmail.com>
2023-06-27 15:21:21 +0100

    Fix missing sound shader

---
+++ addons/hlc_tweaks/CfgSoundShaders.hpp
@@ -0,0 +1,13 @@
+class CfgSoundShaders {
+    class nia_p2269mm_closeShot_SoundShader {
+        range = 20;
+        rangeCurve[] = {
+            {0, 1.5},
+            {20, 0}
+        };
+        volume = "1.5*(1-interior)";
+        samples[] = {
+            {"hlc_wp_p226\snd\p226_close", 1}
+        };
+    };
+};
