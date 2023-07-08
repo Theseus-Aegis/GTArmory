87eb2a31d63cefb88f418895fc82ce056ebd06f6
Mike-MF <TyroneMF@hotmail.com>
2023-07-08 17:23:41 +0100

    Update l85.hpp

--- addons/resound/weapons/l85.hpp
+++ addons/resound/weapons/l85.hpp
@@ -5,14 +5,14 @@ class CUP_L85A2_base: Rifle_Base_F {
             soundSetShot[] = {L85_SOUND_SET};
         };
         class SilencedSound: BaseSoundModeType {
-            soundSetShot[] = {L85_SOUND_SET};
+            soundSetShot[] = {L85S_SOUND_SET};
         };
     };
 
     class FullAuto: Mode_FullAuto {
         class BaseSoundModeType;
         class StandardSound: BaseSoundModeType {
-            soundSetShot[] = {L85S_SOUND_SET};
+            soundSetShot[] = {L85_SOUND_SET};
         };
         class SilencedSound: BaseSoundModeType {
             soundSetShot[] = {L85S_SOUND_SET};
