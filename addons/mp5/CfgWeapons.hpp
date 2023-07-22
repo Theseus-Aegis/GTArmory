4ef434b934982948a09f244388c7ae043de456e8
Mike-MF <TyroneMF@hotmail.com>
2023-07-22 09:59:27 +0100

    Remove Ammunition AI Component (#163)

--- addons/mp5/CfgWeapons.hpp
+++
@@ -1,8 +0,0 @@
-class CfgWeapons {
-    class Rifle_Base_F;
-    class CUP_smg_MP5SD6: Rifle_Base_F {
-        magazineWell[] = {
-            QCLASS(9x19_MP5)
-        };
-    };
-};
--- addons/mp5/README.md
+++
@@ -1,3 +0,0 @@
-# MP5
-
-Alters CUP MP5s to use a custom magwell to prevent invisible magazine models.
