0b16d85d338e0566daafcb63a504e09871660b4d
Mike-MF <TyroneMF@hotmail.com>
2023-11-19 19:06:16 +0000

    Prepare Version 2.4.0

--- addons/rebalance/s4g/script_component.hpp
+++ addons/rebalance/s4g/script_component.hpp
@@ -31,8 +31,64 @@
         }; \
     }
 
+#define MACRO_HITPROTECTION_CPC_ALT \
+    class ItemInfo: ItemInfo { \
+        containerClass = "Supply140"; \
+        mass = 50; \
+        class HitpointsProtectionInfo { \
+            class Chest { \
+                HitpointName = "HitChest"; \
+                armor = 16; \
+                passThrough = 0.38; \
+            }; \
+            class Diaphragm { \
+                HitpointName = "HitDiaphragm"; \
+                armor = 16; \
+                passThrough = 0.38; \
+            }; \
+            class Abdomen { \
+                hitpointName = "HitAbdomen"; \
+                armor = 16; \
+                passThrough = 0.38; \
+            }; \
+            class Body { \
+                hitpointName = "HitBody"; \
+                armor = 16; \
+                passThrough = 0.38; \
+            }; \
+        }; \
+    }
+
 #define MACRO_HITPROTECTION_LBT \
     class ItemInfo: VestItem { \
+        containerClass = "Supply140"; \
+        mass = 50; \
+        class HitpointsProtectionInfo { \
+            class Chest { \
+                HitpointName = "HitChest"; \
+                armor = 16; \
+                passThrough = 0.35; \
+            }; \
+            class Diaphragm { \
+                HitpointName = "HitDiaphragm"; \
+                armor = 16; \
+                passThrough = 0.35; \
+            }; \
+            class Abdomen { \
+                hitpointName = "HitAbdomen"; \
+                armor = 16; \
+                passThrough = 0.35; \
+            }; \
+            class Body { \
+                hitpointName = "HitBody"; \
+                armor = 16; \
+                passThrough = 0.35; \
+            }; \
+        }; \
+    }
+
+#define MACRO_HITPROTECTION_LBT_ALT \
+    class ItemInfo: ItemInfo { \
         containerClass = "Supply140"; \
         mass = 50; \
         class HitpointsProtectionInfo { \
