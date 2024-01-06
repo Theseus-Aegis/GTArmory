ebd4782eb957d8c1bea67f2ce777c0b254b95655
LinkIsGrim <salluci.lovi@gmail.com>
2024-01-06 20:07:24 -0300

    add missing base classes

--- addons/recoils/ws/weapons/baseclasses.hpp
+++ addons/recoils/ws/weapons/baseclasses.hpp
@@ -1,6 +1,8 @@
 class arifle_Galat_base_lxWS;
 class arifle_SLR_V_lxWS;
+class arifle_SPAR_01_base_F;
 class arifle_Velko_base_lxWS;
+class DMR_06_base_F;
 class LMG_S77_base_lxWS;
 class LMG_S77_Compact_base_lxWS;
 class sgun_aa40_base_lxWS;
