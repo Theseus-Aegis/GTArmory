b7d915eac11e5c6ead4f2838cd0d7011834015ae
LinkIsGrim <salluci.lovi@gmail.com>
2024-01-06 20:05:37 -0300

    missing files

--- addons/recoils/bohemia/556.hpp
+++ addons/recoils/bohemia/556.hpp
@@ -49,32 +49,6 @@ class arifle_TRG20_F: Tavor_base_F {
     recoil = QCLASS(556_Medium_BP);
 };
 
-// Velko R4/R5
-class arifle_Velko_lxWS: arifle_Velko_base_lxWS {
-    recoil = QCLASS(556_Long);
-};
-
-class arifle_VelkoR5_lxWS: arifle_Velko_base_lxWS {
-    recoil = QCLASS(556_Short);
-};
-
-class arifle_VelkoR5_GL_lxWS: arifle_VelkoR5_lxWS {
-    recoil = QCLASS(556_Short_FG);
-};
-
-// XMS
-class arifle_XMS_Base_lxWS: arifle_SPAR_01_base_F {
-    recoil = QCLASS(556_Short_BP);
-};
-
-class arifle_XMS_GL_lxWS: arifle_XMS_Base_lxWS  {
-    recoil = QCLASS(556_Short_FG);
-};
-
-class arifle_XMS_Shot_lxWS: arifle_XMS_Base_lxWS {
-    recoil = QCLASS(556_Short_FG);
-};
-
 // Minimi SPW
 class LMG_03_F: LMG_03_base_F {
     recoil = QCLASS(556_Long_MG);
