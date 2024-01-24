d2e9866139b246ce8d18a59b392e6f4e5b0135f1
Mike-MF <TyroneMF@hotmail.com>
2024-01-24 19:26:52 +0000

    Initial Push

---
+++ addons/hidden_units/Vanilla_Units/una.hpp
@@ -0,0 +1,122 @@
+class B_UN_soldier_AR_lxWS: B_soldier_AR_F {
+    scope = 1;
+    scopeCurator = 1;
+};
+class B_UN_HeavyGunner_lxWS: B_UN_soldier_AR_lxWS {
+    scope = 1;
+    scopeCurator = 1;
+};
+class B_UN_Soldier_lxWS: B_Soldier_F {
+    scope = 1;
+    scopeCurator = 1;
+};
+class B_UN_Soldier_unarmed_lxWS: B_UN_Soldier_lxWS {
+    scope = 1;
+    scopeCurator = 1;
+};
+class B_UN_survivor_lxWS: B_UN_Soldier_lxWS {
+    scope = 1;
+    scopeCurator = 1;
+};
+class B_UN_medic_lxWS: B_medic_F {
+    scope = 1;
+    scopeCurator = 1;
+};
+class B_UN_crew_lxWS: B_crew_F {
+    scope = 1;
+    scopeCurator = 1;
+};
+class B_UN_engineer_lxWS: B_engineer_F {
+    scope = 1;
+    scopeCurator = 1;
+};
+class B_UN_Soldier_GL_lxWS: B_Soldier_GL_F {
+    scope = 1;
+    scopeCurator = 1;
+};
+class B_UN_Helipilot_lxWS: B_Helipilot_F {
+    scope = 1;
+    scopeCurator = 1;
+};
+class B_UN_officer_lxWS: B_officer_F {
+    scope = 1;
+    scopeCurator = 1;
+};
+class B_UN_soldier_repair_lxWS: B_soldier_repair_F {
+    scope = 1;
+    scopeCurator = 1;
+};
+class B_UN_Soldier_lite_lxWS: B_Soldier_lite_F {
+    scope = 1;
+    scopeCurator = 1;
+};
+class B_UN_Soldier_TL_lxWS: B_Soldier_TL_F {
+    scope = 1;
+    scopeCurator = 1;
+};
+
+// Vehicles
+class B_UN_APC_Wheeled_01_command_lxWS: APC_Wheeled_01_command_base_lxWS {
+    scope = 1;
+    scopeCurator = 1;
+};
+class B_UNA_APC_Wheeled_02_hmg_lxWS: APC_Wheeled_02_hmg_base_lxws {
+    scope = 1;
+    scopeCurator = 1;
+};
+class B_UNA_APC_Wheeled_02_unarmed_lxWS: APC_Wheeled_02_unarmed_base_lxws {
+    scope = 1;
+    scopeCurator = 1;
+};
+class B_UN_Truck_01_mover_lxWS: B_Truck_01_mover_F {
+    scope = 1;
+    scopeCurator = 1;
+};
+class B_UN_Truck_01_ammo_lxWS: B_Truck_01_ammo_F {
+    scope = 1;
+    scopeCurator = 1;
+};
+class B_UN_Truck_01_box_lxWS: B_Truck_01_box_F {
+    scope = 1;
+    scopeCurator = 1;
+};
+class B_UN_Truck_01_fuel_lxWS: B_Truck_01_fuel_F {
+    scope = 1;
+    scopeCurator = 1;
+};
+class B_UN_Truck_01_medical_lxWS: B_Truck_01_medical_F {
+    scope = 1;
+    scopeCurator = 1;
+};
+class B_UN_Truck_01_Repair_lxWS: B_Truck_01_Repair_F {
+    scope = 1;
+    scopeCurator = 1;
+};
+class B_UN_Truck_01_transport_lxWS: B_Truck_01_transport_F {
+    scope = 1;
+    scopeCurator = 1;
+};
+class B_UN_Truck_01_covered_lxWS: B_Truck_01_covered_F {
+    scope = 1;
+    scopeCurator = 1;
+};
+class B_UN_MRAP_01_lxWS: B_MRAP_01_F {
+    scope = 1;
+    scopeCurator = 1;
+};
+class B_UN_Offroad_lxWS: Offroad_01_base_lxWS {
+    scope = 1;
+    scopeCurator = 1;
+};
+class B_UN_Offroad_Armor_lxWS: Offroad_01_armor_base_lxWS {
+    scope = 1;
+    scopeCurator = 1;
+};
+class B_UN_Heli_Transport_02_lxWS: Heli_Transport_02_base_F {
+    scope = 1;
+    scopeCurator = 1;
+};
+class B_MBT_03_cannon_lxWS: I_MBT_03_cannon_F {
+    scope = 1;
+    scopeCurator = 1;
+};
