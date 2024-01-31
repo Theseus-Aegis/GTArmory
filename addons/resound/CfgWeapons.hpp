a3f24bff43fcb1cc1b7269eb1af3004f0fdd3705
Mike-MF <TyroneMF@hotmail.com>
2024-01-31 09:27:12 +0000

    poosh

--- addons/resound/CfgWeapons.hpp
+++ addons/resound/CfgWeapons.hpp
@@ -5,10 +5,11 @@ class CfgWeapons {
     class LMG_RCWS;
 
     #include "weapons\ak.hpp"
-    #include "weapons\saiga12k.hpp"
     #include "weapons\deagle.hpp"
     #include "weapons\l85.hpp"
     #include "weapons\m1014.hpp"
     #include "weapons\minigun.hpp"
     #include "weapons\mp7.hpp"
+    #include "weapons\saiga12k.hpp"
+    #include "weapons\svd.hpp"
 };
