f19efbbbf2556ff6a369ebb4f075bcbb6cd12767
Mike-MF <TyroneMF@hotmail.com>
2024-03-25 11:26:53 +0000

    HLC Tweaks - Hide all the weird NiArms attachments (#225)
    * Hide all the weird NiArms attachments
    
    * Update singleFire_AUG.hpp

--- addons/hlc_tweaks/CfgWeapons.hpp
+++ addons/hlc_tweaks/CfgWeapons.hpp
@@ -1,97 +1,23 @@
 class CfgWeapons {
+    class muzzle_snds_H;
+    class muzzle_snds_M;
+    class bipod_01_F_snd;
     class ItemCore;
-    class hlc_acc_DBALPL_FL : ItemCore {
+    class hlc_rifle_honeybase;
+    class hlc_aug_base;
+    class hlc_rifle_auga2carb;
+    class hlc_rifle_auga2;
+    class hlc_rifle_auga2lsw;
+
+    #include "hidden_attachments.hpp"
+    #include "hidden_augRifles.hpp"
+    #include "singleFire_AUG.hpp"
+
+    class hlc_acc_DBALPL_FL: ItemCore {
         scope = 1;
     };
 
-    // Enable Semi-Auto for AUGs
-    class hlc_aug_base;
-    class hlc_rifle_aug : hlc_aug_base {
-        class Single: Single {
-            showToPlayer = 1;
-        };
-    };
-
-    class hlc_rifle_auga2: hlc_rifle_aug {
-        class Single: Single {
-            showToPlayer = 1;
-        };
-    };
-
-    class hlc_rifle_auga3: hlc_rifle_aug {
-        class Single: Single {
-            showToPlayer = 1;
-        };
-        class LinkedItems {
-            delete LinkedItemsMuzzle;
-        };
-    };
-
-    class hlc_rifle_auga3_b: hlc_rifle_auga3 {
-        class LinkedItems {
-            delete LinkedItemsMuzzle;
-        };
-    };
-
-    class hlc_rifle_auga3_bl: hlc_rifle_auga3 {
-        class LinkedItems {
-            delete LinkedItemsMuzzle;
-        };
-    };
-
-    // HBAR semi-auto, isn't not using inheritance fun guys?
-    class hlc_rifle_aughbar: hlc_rifle_aug {
-        modes[]= {
-            "Single",
-            "FullAuto",
-            "50",
-            "AI_long",
-            "AI_close",
-            "AI_short",
-            "AI_far",
-            "AI_toofar",
-            "AI_far_optic1"
-        };
-        class Single: Single {
-            showToPlayer = 1;
-        };
-    };
-
-    class hlc_rifle_aughbar_b: hlc_rifle_aughbar {
-        modes[] = {
-            "Single",
-            "FullAuto",
-            "fullauto_medium",
-            "single_medium_optics1",
-            "single_far_optics2"
-        };
-    };
-
-    class hlc_rifle_aughbar_t: hlc_rifle_aughbar {
-        modes[] = {
-            "Single",
-            "FullAuto",
-            "fullauto_medium",
-            "single_medium_optics1",
-            "single_far_optics2"
-        };
-    };
-
-    class hlc_rifle_auga2lsw: hlc_rifle_aughbar {
-        modes[]= {
-            "Single",
-            "FullAuto",
-            "50",
-            "AI_long",
-            "AI_close",
-            "AI_short",
-            "AI_far",
-            "AI_toofar",
-            "AI_far_optic1"
-        };
-    };
-
-    class hlc_rifle_honeybase;
+    // Add linked suppressor.
     class hlc_rifle_honeybadger: hlc_rifle_honeybase {
         class LinkedItems {
             class LinkedItemsMuzzle {
