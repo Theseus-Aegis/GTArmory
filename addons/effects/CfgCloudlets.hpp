208b7eee5f7ceadabfec5c945dbd19ef092a4649
Mike-MF <TyroneMF@hotmail.com>
2024-03-25 19:03:32 +0000

    Alphabetical

--- addons/effects/CfgCloudlets.hpp
+++ addons/effects/CfgCloudlets.hpp
@@ -58,16 +58,16 @@ class CfgCloudlets {
     // Blood Effects
     class Blood;
     class CLASS(bloodMist): Blood {
-        particleShape = "\A3\data_f\ParticleEffects\Universal\Universal_02";
-        particleFSNtieth = 8;
-        particleFSIndex = 4;
-        particleFSFrameCount = 1;
-        particleFSLoop = 0;
-        lifeTime = 0.5;
-        size[] = {"((hit factor [3,100])/50 + 0.5)", "2*((hit factor [3,100])/50 + 0.5)"};
         color[] = {{0.7,0,0,0.2}, {0.7,0,0,0.1}};
-        randomDirectionPeriod = 0.3;
+        lifeTime = 0.5;
+        particleFSFrameCount = 1;
+        particleFSIndex = 4;
+        particleFSLoop = 0;
+        particleFSNtieth = 8;
+        particleShape = "\A3\data_f\ParticleEffects\Universal\Universal_02";
         randomDirectionIntensity = 0.3;
+        randomDirectionPeriod = 0.3;
+        size[] = {"((hit factor [3,100])/50 + 0.5)", "2*((hit factor [3,100])/50 + 0.5)"};
         weight = 0.127;
     };
 
