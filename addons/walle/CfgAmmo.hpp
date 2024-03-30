3907c7a37c96283a206518d35f089521f1298e83
Mike-MF <TyroneMF@hotmail.com>
2024-03-30 17:46:42 +0000

    Possibly fix WallE

--- addons/walle/CfgAmmo.hpp
+++ addons/walle/CfgAmmo.hpp
@@ -2,9 +2,9 @@ class CfgAmmo {
     class B_12Gauge_Pellets_Submunition;
     class CLASS(Diffuser_Ammo): B_12Gauge_Pellets_Submunition {
         aiAmmoUsageFlags = 0; // Should prevent it from shooting at AI automatically.
-        submunitionAmmo = "B_12Gauge_Pellets_Submunition";
+        submunitionAmmo = QCLASS(12g_Pellets_Submunition_Deploy);
         submunitionConeAngle = 0.22;
         submunitionConeAngleHorizontal = 0.22;
-        submunitionConeType[] = {"poissondisccenter", 20};
+        submunitionConeType[] = {"poissondisccenter", 10};
     };
 };
