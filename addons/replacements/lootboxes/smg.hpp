022876e4539ae74567df51a5dbc604479b40cf69
Mike-MF <TyroneMF@hotmail.com>
2024-04-27 22:29:14 +0100

    Update lootboxes

--- addons/replacements/lootboxes/smg.hpp
+++ addons/replacements/lootboxes/smg.hpp
@@ -3,8 +3,16 @@ class CLASS(Lootbox_SMG): tac_supplies_randomizedBox {
     picture = QPATHTOF(ui\lootbox_smg_ca.paa);
     displayName = "Lootbox (SMG)";
     tac_supplies_possibleItems[] = {
-        "CUP_arifle_SR3M_Vikhr_VFG_top_rail",
-        "CUP_smg_bizon",
+        "hgun_PDW2000_F",
+        "SMG_05_F",
+        "SMG_03C_TR_black",
+        "SMG_03C_TR_camo",
+        "SMG_03C_TR_khaki",
+        "SMG_03_TR_black",
+        "SMG_03_TR_camo",
+        "SMG_03_TR_khaki",
+        "SMG_02_F",
+        "SMG_01_F",
         "CUP_smg_MP5A5",
         "CUP_smg_MP5A5_Rail",
         "CUP_smg_MP5A5_Rail_AFG",
@@ -12,17 +20,9 @@ class CLASS(Lootbox_SMG): tac_supplies_randomizedBox {
         "CUP_smg_MP5SD6",
         "CUP_smg_MP7",
         "CUP_smg_MP7_desert",
+        "CUP_smg_bizon",
+        "CUP_arifle_SR3M_Vikhr_VFG_top_rail",
         "CUP_smg_vityaz_top_rail",
-        "CUP_smg_vityaz_vfg_top_rail",
-        "hgun_PDW2000_F",
-        "SMG_01_F",
-        "SMG_02_F",
-        "SMG_03_TR_black",
-        "SMG_03_TR_camo",
-        "SMG_03_TR_khaki",
-        "SMG_03C_TR_black",
-        "SMG_03C_TR_camo",
-        "SMG_03C_TR_khaki",
-        "SMG_05_F"
+        "CUP_smg_vityaz_vfg_top_rail"
     };
 };
