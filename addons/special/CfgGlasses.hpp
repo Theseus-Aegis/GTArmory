c5a69ef901d8c3705c1a799ea77c1408c292cab8
Mike-MF <TyroneMF@hotmail.com>
2024-05-22 20:35:51 +0100

    Special Beard

---
+++ addons/special/CfgGlasses.hpp
@@ -0,0 +1,8 @@
+class CfgGlasses {
+    class CUP_G_Beard_Shades_Blonde;
+    class CLASS(Custom_Mathews_Beard): CUP_G_Beard_Shades_Blonde {
+        displayName = "MilSpec Mick (Beard/Glasses)";
+        hiddenSelections[] = {"Camo4"};
+        hiddenSelectionsTextures[] = {QPATHTOF(data\custom_mathews_beard_co.paa)};
+    };
+};
