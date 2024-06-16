1bfdb233ae629691f20c8ae0a420305765a098f3
Mike-MF <TyroneMF@hotmail.com>
2024-06-16 14:34:00 +0100

    Hidden Units - RF / WS Compat Data (#253)

---
+++ addons/hidden_units/rf_ws/CfgVehicles.hpp
@@ -0,0 +1,61 @@
+class CfgVehicles {
+    class Pickup_01_base_rf;
+    class Pickup_01_mmg_base_rf;
+    class Pickup_01_hmg_base_rf;
+    class Pickup_fuel_base_rf;
+    class Heli_EC_01A_military_base_RF;
+    class I_SFIA_Heli_EC_02_RF;
+
+    class B_UN_Pickup_rf: Pickup_01_base_rf {
+        scope = 1;
+        scopeCurator = 1;
+    };
+    class B_UN_Pickup_mmg_rf: Pickup_01_mmg_base_rf {
+        scope = 1;
+        scopeCurator = 1;
+    };
+    class I_Tura_Pickup_01_RF: Pickup_01_base_rf {
+        scope = 1;
+        scopeCurator = 1;
+    };
+    class I_Tura_Pickup_fuel_rf: Pickup_fuel_base_rf {
+        scope = 1;
+        scopeCurator = 1;
+    };
+    class I_Tura_Pickup_01_hmg_rf: Pickup_01_hmg_base_rf {
+        scope = 1;
+        scopeCurator = 1;
+    };
+    class B_Tura_Pickup_01_RF: I_Tura_Pickup_01_RF {
+        scope = 1;
+        scopeCurator = 1;
+    };
+    class B_Tura_Pickup_01_fuel_RF: I_Tura_Pickup_fuel_rf {
+        scope = 1;
+        scopeCurator = 1;
+    };
+    class B_Tura_Pickup_01_hmg_RF: I_Tura_Pickup_01_hmg_rf {
+        scope = 1;
+        scopeCurator = 1;
+    };
+    class O_Tura_Pickup_01_RF: I_Tura_Pickup_01_RF {
+        scope = 1;
+        scopeCurator = 1;
+    };
+    class O_Tura_Pickup_01_fuel_RF: I_Tura_Pickup_fuel_rf {
+        scope = 1;
+        scopeCurator = 1;
+    };
+    class O_Tura_Pickup_01_hmg_RF: I_Tura_Pickup_01_hmg_rf {
+        scope = 1;
+        scopeCurator = 1;
+    };
+    class B_UN_Heli_EC_01A_military_RF: Heli_EC_01A_military_base_RF {
+        scope = 1;
+        scopeCurator = 1;
+    };
+    class O_SFIA_Heli_EC_02_RF: I_SFIA_Heli_EC_02_RF {
+        scope = 1;
+        scopeCurator = 1;
+    };
+};
---
+++ addons/hidden_units/rf_ws/script_component.hpp
@@ -0,0 +1,4 @@
+#define COMPONENT hidden_units_rfws
+#define COMPONENT_BEAUTIFIED Hidden Units RF/WS
+#include "\x\tacgt\addons\main\script_mod.hpp"
+#include "\x\tacgt\addons\main\script_macros.hpp"
