692b898d3a174d79eb78740198b414d5879811e2
Mike-MF <TyroneMF@hotmail.com>
2024-12-31 15:27:28 +0000

    Hidden Units - Switch to macro

--- addons/hidden_units/Vanilla_Units/ctrg.hpp
+++ addons/hidden_units/Vanilla_Units/ctrg.hpp
@@ -1,42 +1,32 @@
 // CTRG - Units (including WS Desert Units)
 
 class B_CTRG_Soldier_AR_tna_F: B_CTRG_Soldier_F {
-    scope = 1;
-    scopeCurator = 1;
+    MACRO_HIDE_VEHICLE;
 };
 class B_CTRG_Soldier_Exp_tna_F: B_CTRG_Soldier_F {
-    scope = 1;
-    scopeCurator = 1;
+    MACRO_HIDE_VEHICLE;
 };
 class B_CTRG_Soldier_JTAC_tna_F: B_CTRG_Soldier_F {
-    scope = 1;
-    scopeCurator = 1;
+    MACRO_HIDE_VEHICLE;
 };
 class B_CTRG_Soldier_M_tna_F: B_CTRG_Soldier_F {
-    scope = 1;
-    scopeCurator = 1;
+    MACRO_HIDE_VEHICLE;
 };
 class B_CTRG_Soldier_Medic_tna_F: B_CTRG_Soldier_F {
-    scope = 1;
-    scopeCurator = 1;
+    MACRO_HIDE_VEHICLE;
 };
 class B_CTRG_Soldier_LAT2_tna_F: B_CTRG_Soldier_F {
-    scope = 1;
-    scopeCurator = 1;
+    MACRO_HIDE_VEHICLE;
 };
 class B_CTRG_Soldier_tna_F: B_CTRG_Soldier_F {
-    scope = 1;
-    scopeCurator = 1;
+    MACRO_HIDE_VEHICLE;
 };
 class B_CTRG_Soldier_LAT_tna_F: B_CTRG_Soldier_F {
-    scope = 1;
-    scopeCurator = 1;
+    MACRO_HIDE_VEHICLE;
 };
 class B_CTRG_Soldier_TL_tna_F: B_CTRG_Soldier_F {
-    scope = 1;
-    scopeCurator = 1;
+    MACRO_HIDE_VEHICLE;
 };
 class B_CTRG_Miller_F: B_CTRG_Soldier_3_F {
-    scope = 1;
-    scopeCurator = 1;
+    MACRO_HIDE_VEHICLE;
 };
