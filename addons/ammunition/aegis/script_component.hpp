17fccb1d97265fe6047681c00d1376c301c52c8a
Mike-MF <TyroneMF@hotmail.com>
2024-12-31 19:53:26 +0000

    Fix typo

--- addons/ammunition/aegis/script_component.hpp
+++ addons/ammunition/aegis/script_component.hpp
@@ -1,4 +1,4 @@
-#define COMPONENT ammunition_aeigs
+#define COMPONENT ammunition_aegis
 #define COMPONENT_BEAUTIFIED Ammunition Aegis
 #include "\x\tacgt\addons\main\script_mod.hpp"
 #include "\x\tacgt\addons\main\script_macros.hpp"
