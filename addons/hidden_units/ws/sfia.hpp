b9ac4923457f0a72d0b9a6f7aa892b5dc170bf8e
Mike-MF <TyroneMF@hotmail.com>
2024-12-31 15:28:49 +0000

    Hidden Units - Switch to macro (#276)

--- addons/hidden_units/ws/sfia.hpp
+++ addons/hidden_units/ws/sfia.hpp
@@ -1,429 +1,324 @@
 // Units - SFIA
 class I_SFIA_soldier_lxWS: I_soldier_F {
-    scope = 1;
-    scopeCurator = 1;
+    MACRO_HIDE_VEHICLE;
 };
 class I_SFIA_Soldier_TL_lxWS: I_Soldier_TL_F {
-    scope = 1;
-    scopeCurator = 1;
+    MACRO_HIDE_VEHICLE;
 };
 class I_SFIA_Soldier_GL_lxWS: I_Soldier_GL_F {
-    scope = 1;
-    scopeCurator = 1;
+    MACRO_HIDE_VEHICLE;
 };
 class I_SFIA_Soldier_AR_lxWS: I_Soldier_AR_F {
-    scope = 1;
-    scopeCurator = 1;
+    MACRO_HIDE_VEHICLE;
 };
 class I_SFIA_officer_lxWS: I_officer_F {
-    scope = 1;
-    scopeCurator = 1;
+    MACRO_HIDE_VEHICLE;
 };
 class I_SFIA_sharpshooter_lxWS: I_G_Sharpshooter_F {
-    scope = 1;
-    scopeCurator = 1;
+    MACRO_HIDE_VEHICLE;
 };
 class I_SFIA_crew_lxWS: I_crew_F {
-    scope = 1;
-    scopeCurator = 1;
+    MACRO_HIDE_VEHICLE;
 };
 class I_SFIA_pilot_lxWS: I_pilot_F {
-    scope = 1;
-    scopeCurator = 1;
+    MACRO_HIDE_VEHICLE;
 };
 class I_SFIA_soldier_at_lxWS: I_Soldier_AT_F {
-    scope = 1;
-    scopeCurator = 1;
+    MACRO_HIDE_VEHICLE;
 };
 class I_SFIA_soldier_aa_lxWS: I_Soldier_AA_F {
-    scope = 1;
-    scopeCurator = 1;
+    MACRO_HIDE_VEHICLE;
 };
 class I_SFIA_medic_lxWS: I_G_medic_F {
-    scope = 1;
-    scopeCurator = 1;
+    MACRO_HIDE_VEHICLE;
 };
 class I_SFIA_exp_lxWS: I_G_Soldier_exp_F {
-    scope = 1;
-    scopeCurator = 1;
+    MACRO_HIDE_VEHICLE;
 };
 class I_SFIA_repair_lxWS: I_Soldier_repair_F {
-    scope = 1;
-    scopeCurator = 1;
+    MACRO_HIDE_VEHICLE;
 };
 class I_SFIA_Soldier_AAT_lxWS: I_Soldier_AAT_F {
-    scope = 1;
-    scopeCurator = 1;
+    MACRO_HIDE_VEHICLE;
 };
 class I_SFIA_Soldier_AAA_lxWS: I_Soldier_AAA_F {
-    scope = 1;
-    scopeCurator = 1;
+    MACRO_HIDE_VEHICLE;
 };
 class I_SFIA_soldier_lite_lxWS: I_SFIA_soldier_lxWS {
-    scope = 1;
-    scopeCurator = 1;
+    MACRO_HIDE_VEHICLE;
 };
 class I_SFIA_defector_lxWS: I_SFIA_soldier_lxWS {
-    scope = 1;
-    scopeCurator = 1;
+    MACRO_HIDE_VEHICLE;
 };
 class I_Tura_deserter_lxWS: I_SFIA_deserter_lxWS {
-    scope = 1;
-    scopeCurator = 1;
+    MACRO_HIDE_VEHICLE;
 };
 class I_SFIA_enforcer_lxWS: I_C_Soldier_Para_2_F {
-    scope = 1;
-    scopeCurator = 1;
+    MACRO_HIDE_VEHICLE;
 };
 class I_SFIA_hireling_lxWS: I_C_Soldier_Bandit_2_F {
-    scope = 1;
-    scopeCurator = 1;
+    MACRO_HIDE_VEHICLE;
 };
 class I_Tura_HeavyGunner_lxWS: I_Tura_deserter_lxWS {
-    scope = 1;
-    scopeCurator = 1;
+    MACRO_HIDE_VEHICLE;
 };
 class I_SFIA_scout_lxWS: I_C_Soldier_Bandit_5_F {
-    scope = 1;
-    scopeCurator = 1;
+    MACRO_HIDE_VEHICLE;
 };
 class I_SFIA_medic2_lxWS: I_C_Soldier_Bandit_1_F {
-    scope = 1;
-    scopeCurator = 1;
+    MACRO_HIDE_VEHICLE;
 };
 class I_SFIA_thug_lxWS: I_SFIA_scout_lxWS {
-    scope = 1;
-    scopeCurator = 1;
+    MACRO_HIDE_VEHICLE;
 };
 class I_Tura_soldier_UAV_lxWS: I_Tura_deserter_lxWS {
-    scope = 1;
-    scopeCurator = 1;
+    MACRO_HIDE_VEHICLE;
 };
 class I_SFIA_watcher_lxWS: I_C_Soldier_Bandit_4_F {
-    scope = 1;
-    scopeCurator = 1;
+    MACRO_HIDE_VEHICLE;
 };
 class I_SFIA_Said_lxWS: I_SFIA_officer_lxWS {
-    scope = 1;
-    scopeCurator = 1;
+    MACRO_HIDE_VEHICLE;
 };
 class I_PMC_Story_Gustavo_lxWS: I_PMC_Soldier_01_lxWS {
-    scope = 1;
-    scopeCurator = 1;
+    MACRO_HIDE_VEHICLE;
 };
 class I_Tura_HMG_02_lxWS: I_SFIA_HMG_02_lxWS {
-    scope = 1;
-    scopeCurator = 1;
+    MACRO_HIDE_VEHICLE;
 };
 class I_Tura_HMG_02_high_lxWS: I_SFIA_HMG_02_high_lxWS {
-    scope = 1;
-    scopeCurator = 1;
+    MACRO_HIDE_VEHICLE;
 };
 class I_Tura_Mortar_lxWS: I_SFIA_Mortar_lxWS {
-    scope = 1;
-    scopeCurator = 1;
+    MACRO_HIDE_VEHICLE;
 };
 class I_Tura_Offroad_armor_lxWS: Offroad_01_armor_base_lxWS {
-    scope = 1;
-    scopeCurator = 1;
+    MACRO_HIDE_VEHICLE;
 };
 class I_Tura_Offroad_armor_AT_lxWS: Offroad_01_armor_AT_lxWS {
-    scope = 1;
-    scopeCurator = 1;
+    MACRO_HIDE_VEHICLE;
 };
 class I_Tura_Offroad_armor_armed_lxWS: Offroad_01_armor_armed_lxWS {
-    scope = 1;
-    scopeCurator = 1;
+    MACRO_HIDE_VEHICLE;
 };
 class I_Tura_UAV_02_IED_lxWS: UAV_02_IED_Base_lxWS {
-    scope = 1;
-    scopeCurator = 1;
+    MACRO_HIDE_VEHICLE;
 };
 
 // Opfor
 class O_SFIA_Soldier_AAA_lxWS: I_SFIA_Soldier_AAA_lxWS {
-    scope = 1;
-    scopeCurator = 1;
+    MACRO_HIDE_VEHICLE;
 };
 class O_SFIA_Soldier_AAT_lxWS: I_SFIA_Soldier_AAT_lxWS {
-    scope = 1;
-    scopeCurator = 1;
+    MACRO_HIDE_VEHICLE;
 };
 class O_SFIA_Soldier_AR_lxWS: I_SFIA_Soldier_AR_lxWS {
-    scope = 1;
-    scopeCurator = 1;
+    MACRO_HIDE_VEHICLE;
 };
 class O_SFIA_medic_lxWS: I_SFIA_medic_lxWS {
-    scope = 1;
-    scopeCurator = 1;
+    MACRO_HIDE_VEHICLE;
 };
 class O_SFIA_crew_lxWS: I_SFIA_crew_lxWS {
-    scope = 1;
-    scopeCurator = 1;
+    MACRO_HIDE_VEHICLE;
 };
 class O_SFIA_exp_lxWS: I_SFIA_exp_lxWS {
-    scope = 1;
-    scopeCurator = 1;
+    MACRO_HIDE_VEHICLE;
 };
 class O_SFIA_Soldier_GL_lxWS: I_SFIA_Soldier_GL_lxWS {
-    scope = 1;
-    scopeCurator = 1;
+    MACRO_HIDE_VEHICLE;
 };
 class O_SFIA_HeavyGunner_lxWS: O_SFIA_Soldier_AR_lxWS {
-    scope = 1;
-    scopeCurator = 1;
+    MACRO_HIDE_VEHICLE;
 };
 class O_SFIA_soldier_aa_lxWS: I_SFIA_soldier_aa_lxWS {
-    scope = 1;
-    scopeCurator = 1;
+    MACRO_HIDE_VEHICLE;
 };
 class O_SFIA_soldier_at_lxWS: I_SFIA_soldier_at_lxWS {
-    scope = 1;
-    scopeCurator = 1;
+    MACRO_HIDE_VEHICLE;
 };
 class O_SFIA_officer_lxWS: I_SFIA_officer_lxWS {
-    scope = 1;
-    scopeCurator = 1;
+    MACRO_HIDE_VEHICLE;
 };
 class O_SFIA_pilot_lxWS: I_SFIA_pilot_lxWS {
-    scope = 1;
-    scopeCurator = 1;
+    MACRO_HIDE_VEHICLE;
 };
 class O_SFIA_repair_lxWS: I_SFIA_repair_lxWS {
-    scope = 1;
-    scopeCurator = 1;
+    MACRO_HIDE_VEHICLE;
 };
 class O_SFIA_soldier_lxWS: I_SFIA_soldier_lxWS {
-    scope = 1;
-    scopeCurator = 1;
+    MACRO_HIDE_VEHICLE;
 };
 class O_SFIA_soldier_lite_lxWS: O_SFIA_soldier_lxWS {
-    scope = 1;
-    scopeCurator = 1;
+    MACRO_HIDE_VEHICLE;
 };
 class O_SFIA_soldier_unarmed_lxWS: I_SFIA_soldier_unarmed_lxWS {
-    scope = 1;
-    scopeCurator = 1;
+    MACRO_HIDE_VEHICLE;
 };
 class O_SFIA_sharpshooter_lxWS: I_SFIA_sharpshooter_lxWS {
-    scope = 1;
-    scopeCurator = 1;
+    MACRO_HIDE_VEHICLE;
 };
 class O_SFIA_survivor_lxWS: I_SFIA_survivor_lxWS {
-    scope = 1;
-    scopeCurator = 1;
+    MACRO_HIDE_VEHICLE;
 };
 class O_SFIA_Soldier_TL_lxWS: I_SFIA_Soldier_TL_lxWS {
-    scope = 1;
-    scopeCurator = 1;
+    MACRO_HIDE_VEHICLE;
 };
 class O_Tura_defector_lxWS: I_SFIA_defector_lxWS {
-    scope = 1;
-    scopeCurator = 1;
+    MACRO_HIDE_VEHICLE;
 };
 class O_Tura_deserter_lxWS: I_SFIA_deserter_lxWS {
-    scope = 1;
-    scopeCurator = 1;
+    MACRO_HIDE_VEHICLE;
 };
 class O_Tura_enforcer_lxWS: I_SFIA_enforcer_lxWS {
-    scope = 1;
-    scopeCurator = 1;
+    MACRO_HIDE_VEHICLE;
 };
 class O_Tura_hireling_lxWS: I_SFIA_hireling_lxWS {
-    scope = 1;
-    scopeCurator = 1;
+    MACRO_HIDE_VEHICLE;
 };
 class O_Tura_HeavyGunner_lxWS: I_Tura_HeavyGunner_lxWS {
-    scope = 1;
-    scopeCurator = 1;
+    MACRO_HIDE_VEHICLE;
 };
 class O_Tura_scout_lxWS: I_SFIA_scout_lxWS {
-    scope = 1;
-    scopeCurator = 1;
+    MACRO_HIDE_VEHICLE;
 };
 class O_Tura_medic2_lxWS: I_SFIA_medic2_lxWS {
-    scope = 1;
-    scopeCurator = 1;
+    MACRO_HIDE_VEHICLE;
 };
 class O_Tura_thug_lxWS: I_SFIA_thug_lxWS {
-    scope = 1;
-    scopeCurator = 1;
+    MACRO_HIDE_VEHICLE;
 };
 class O_Tura_soldier_UAV_lxWS: O_Tura_deserter_lxWS {
-    scope = 1;
-    scopeCurator = 1;
+    MACRO_HIDE_VEHICLE;
 };
 class O_Tura_watcher_lxWS: I_SFIA_watcher_lxWS {
-    scope = 1;
-    scopeCurator = 1;
+    MACRO_HIDE_VEHICLE;
 };
 
 // Vehicles
 class O_SFIA_Truck_02_aa_lxWS: I_SFIA_Truck_02_aa_lxWS {
-    scope = 1;
-    scopeCurator = 1;
+    MACRO_HIDE_VEHICLE;
 };
 class O_SFIA_APC_Tracked_02_AA_lxWS: I_SFIA_APC_Tracked_02_AA_lxWS {
-    scope = 1;
-    scopeCurator = 1;
+    MACRO_HIDE_VEHICLE;
 };
 class O_SFIA_APC_Tracked_02_cannon_lxWS: I_SFIA_APC_Tracked_02_cannon_lxWS {
-    scope = 1;
-    scopeCurator = 1;
+    MACRO_HIDE_VEHICLE;
 };
 class O_SFIA_APC_Tracked_02_30mm_lxWS: I_SFIA_APC_Tracked_02_30mm_lxWS {
-    scope = 1;
-    scopeCurator = 1;
+    MACRO_HIDE_VEHICLE;
 };
 class O_SFIA_APC_Wheeled_02_hmg_lxWS: I_SFIA_APC_Wheeled_02_hmg_lxWS {
-    scope = 1;
-    scopeCurator = 1;
+    MACRO_HIDE_VEHICLE;
 };
 class O_SFIA_APC_Wheeled_02_unarmed_lxWS: I_SFIA_APC_Wheeled_02_unarmed_lxWS {
-    scope = 1;
-    scopeCurator = 1;
+    MACRO_HIDE_VEHICLE;
 };
 class O_SFIA_Truck_02_MRL_lxWS: I_SFIA_Truck_02_MRL_lxWS {
-    scope = 1;
-    scopeCurator = 1;
+    MACRO_HIDE_VEHICLE;
 };
 class O_SFIA_Truck_02_fuel_lxWS: I_SFIA_Truck_02_fuel_lxWS {
-    scope = 1;
-    scopeCurator = 1;
+    MACRO_HIDE_VEHICLE;
 };
 class O_SFIA_Truck_02_transport_lxWS: I_SFIA_Truck_02_transport_lxWS {
-    scope = 1;
-    scopeCurator = 1;
+    MACRO_HIDE_VEHICLE;
 };
 class O_SFIA_Truck_02_covered_lxWS: I_SFIA_Truck_02_covered_lxWS {
-    scope = 1;
-    scopeCurator = 1;
+    MACRO_HIDE_VEHICLE;
 };
 class O_SFIA_Offroad_lxWS: I_SFIA_Offroad_lxWS {
-    scope = 1;
-    scopeCurator = 1;
+    MACRO_HIDE_VEHICLE;
 };
 class O_SFIA_Offroad_AT_lxWS: I_SFIA_Offroad_AT_lxWS {
-    scope = 1;
-    scopeCurator = 1;
+    MACRO_HIDE_VEHICLE;
 };
 class O_SFIA_Offroad_armed_lxWS: I_SFIA_Offroad_armed_lxWS {
-    scope = 1;
-    scopeCurator = 1;
+    MACRO_HIDE_VEHICLE;
 };
 class O_SFIA_Truck_02_Ammo_lxWS: I_SFIA_Truck_02_Ammo_lxWS {
-    scope = 1;
-    scopeCurator = 1;
+    MACRO_HIDE_VEHICLE;
 };
 class O_SFIA_Truck_02_cargo_lxWS: I_SFIA_Truck_02_cargo_lxWS {
-    scope = 1;
-    scopeCurator = 1;
+    MACRO_HIDE_VEHICLE;
 };
 class O_SFIA_Truck_02_flatbed_lxWS: I_SFIA_Truck_02_flatbed_lxWS {
-    scope = 1;
-    scopeCurator = 1;
+    MACRO_HIDE_VEHICLE;
 };
 class O_SFIA_Truck_02_box_lxWS: I_SFIA_Truck_02_box_lxWS {
-    scope = 1;
-    scopeCurator = 1;
+    MACRO_HIDE_VEHICLE;
 };
 class O_SFIA_Heli_Attack_02_dynamicLoadout_lxWS: I_SFIA_Heli_Attack_02_dynamicLoadout_lxWS {
-    scope = 1;
-    scopeCurator = 1;
+    MACRO_HIDE_VEHICLE;
 };
 class O_SFIA_MBT_02_cannon_lxWS: I_SFIA_MBT_02_cannon_lxWS {
-    scope = 1;
-    scopeCurator = 1;
+    MACRO_HIDE_VEHICLE;
 };
 class O_SFIA_HMG_02_lxWS: I_SFIA_HMG_02_lxWS {
-    scope = 1;
-    scopeCurator = 1;
+    MACRO_HIDE_VEHICLE;
 };
 class O_SFIA_HMG_02_high_lxWS: I_SFIA_HMG_02_high_lxWS {
-    scope = 1;
-    scopeCurator = 1;
+    MACRO_HIDE_VEHICLE;
 };
 class O_SFIA_Mortar_lxWS: I_SFIA_Mortar_lxWS {
-    scope = 1;
-    scopeCurator = 1;
+    MACRO_HIDE_VEHICLE;
 };
 class O_SFIA_ZU23_lxWS: I_SFIA_ZU23_lxWS {
-    scope = 1;
-    scopeCurator = 1;
+    MACRO_HIDE_VEHICLE;
 };
 class O_Tura_Offroad_armor_lxWS: I_Tura_Offroad_armor_lxWS {
-    scope = 1;
-    scopeCurator = 1;
+    MACRO_HIDE_VEHICLE;
 };
 class O_Tura_Offroad_armor_AT_lxWS: I_Tura_Offroad_armor_AT_lxWS {
-    scope = 1;
-    scopeCurator = 1;
+    MACRO_HIDE_VEHICLE;
 };
 class O_Tura_Offroad_armor_armed_lxWS: I_Tura_Offroad_armor_armed_lxWS {
-    scope = 1;
-    scopeCurator = 1;
+    MACRO_HIDE_VEHICLE;
 };
 class O_Tura_UAV_02_IED_lxWS: UAV_02_IED_Base_lxWS {
-    scope = 1;
-    scopeCurator = 1;
+    MACRO_HIDE_VEHICLE;
 };
 
 // Blufor
 class B_Tura_defector_lxWS: I_SFIA_defector_lxWS {
-    scope = 1;
-    scopeCurator = 1;
+    MACRO_HIDE_VEHICLE;
 };
 class B_Tura_deserter_lxWS: I_SFIA_deserter_lxWS {
-    scope = 1;
-    scopeCurator = 1;
+    MACRO_HIDE_VEHICLE;
 };
 class B_Tura_enforcer_lxWS: I_SFIA_enforcer_lxWS {
-    scope = 1;
-    scopeCurator = 1;
+    MACRO_HIDE_VEHICLE;
 };
 class B_Tura_hireling_lxWS: I_SFIA_hireling_lxWS {
-    scope = 1;
-    scopeCurator = 1;
+    MACRO_HIDE_VEHICLE;
 };
 class B_Tura_HeavyGunner_lxWS: I_Tura_HeavyGunner_lxWS {
-    scope = 1;
-    scopeCurator = 1;
+    MACRO_HIDE_VEHICLE;
 };
 class B_Tura_scout_lxWS: I_SFIA_scout_lxWS {
-    scope = 1;
-    scopeCurator = 1;
+    MACRO_HIDE_VEHICLE;
 };
 class B_Tura_medic2_lxWS: I_SFIA_medic2_lxWS {
-    scope = 1;
-    scopeCurator = 1;
+    MACRO_HIDE_VEHICLE;
 };
 class B_Tura_thug_lxWS: I_SFIA_thug_lxWS {
-    scope = 1;
-    scopeCurator = 1;
+    MACRO_HIDE_VEHICLE;
 };
 class B_Tura_soldier_UAV_lxWS: B_Tura_deserter_lxWS {
-    scope = 1;
-    scopeCurator = 1;
+    MACRO_HIDE_VEHICLE;
 };
 class B_Tura_watcher_lxWS: I_SFIA_watcher_lxWS {
-    scope = 1;
-    scopeCurator = 1;
+    MACRO_HIDE_VEHICLE;
 };
 
 // Vehicles
 class B_Tura_Offroad_armor_lxWS: I_Tura_Offroad_armor_lxWS {
-    scope = 1;
-    scopeCurator = 1;
+    MACRO_HIDE_VEHICLE;
 };
 class B_Tura_Offroad_armor_AT_lxWS: I_Tura_Offroad_armor_AT_lxWS {
-    scope = 1;
-    scopeCurator = 1;
+    MACRO_HIDE_VEHICLE;
 };
 class B_Tura_Offroad_armor_armed_lxWS: I_Tura_Offroad_armor_armed_lxWS {
-    scope = 1;
-    scopeCurator = 1;
+    MACRO_HIDE_VEHICLE;
 };
 class B_Tura_UAV_02_IED_lxWS: UAV_02_IED_Base_lxWS {
-    scope = 1;
-    scopeCurator = 1;
+    MACRO_HIDE_VEHICLE;
 };
