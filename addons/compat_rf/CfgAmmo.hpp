f42fe8aff7ec68871a10cb87c8a58cbc9cd84bfa
Mike-MF <TyroneMF@hotmail.com>
2025-02-23 01:16:46 +0000

    Enable fragmentation for PSRL frag round

---
+++ addons/compat_rf/CfgAmmo.hpp
@@ -0,0 +1,12 @@
+class CfgAmmo {
+    class R_PSRL1_AT_RF;
+    class R_PSRL1_FRAG_RF: R_PSRL1_AT_RF {
+        ace_frag_skip = 0;
+        ace_frag_charge = 210;
+        ace_frag_classes[] = {"ACE_frag_medium"};
+        ace_frag_enabled = 1;
+        ace_frag_gurney_c = 2800;
+        ace_Frag_gurney_k = "3/5";
+        ace_frag_metal = 350;
+    };
+};
