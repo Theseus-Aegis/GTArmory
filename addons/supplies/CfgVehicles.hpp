b54ebb6a3cbd505da148b4bcff5ae4a6e52a5e91
Mike-MF <TyroneMF@hotmail.com>
2025-03-17 17:19:31 +0000

    Keres Box supplies

--- addons/supplies/CfgVehicles.hpp
+++ addons/supplies/CfgVehicles.hpp
@@ -231,10 +231,70 @@ class CfgVehicles {
             QPATHTOF(data\supply_base_texture_co.paa),
             QPATHTOF(data\supply_keres_ca.paa)
         };
-        class TransportMagazines {};
-        class TransportItems {};
+        class TransportMagazines {
+            MACRO_ADDMAGAZINE(ACE_HandFlare_White,10);
+            MACRO_ADDMAGAZINE(CUP_30Rnd_9x19AP_Vityaz,20);
+            MACRO_ADDMAGAZINE(SmokeShellBlue,24);
+            MACRO_ADDMAGAZINE(tacgt_30Rnd_9x19_EPR,20);
+            MACRO_ADDMAGAZINE(tacgt_30Rnd_9x19_EPR_MP5,30);
+            MACRO_ADDMAGAZINE(tacgt_33Rnd_9x19_Ball_Mag_RF,12);
+            MACRO_ADDMAGAZINE(tacgt_65Rnd_9x19_Ball_Mag_RF,2);
+            MACRO_ADDMAGAZINE(tacgt_8Rnd_AA40_000,24);
+            MACRO_ADDMAGAZINE(tacgt_8Rnd_AA40_AP20,16);
+        };
+        class TransportItems {
+            MACRO_ADDITEM(ACE_bodyBag_blue,20);
+            MACRO_ADDITEM(ACE_CableTie,20);
+            MACRO_ADDITEM(ace_earplugs,10);
+            MACRO_ADDITEM(ACE_fieldDressing,100);
+            MACRO_ADDITEM(ACE_painkillers,10);
+            MACRO_ADDITEM(ACE_splint,10);
+            MACRO_ADDITEM(ACE_SpraypaintBlue,10);
+            MACRO_ADDITEM(ACE_tourniquet,20);
+            MACRO_ADDITEM(ACRE_PRC152,10);
+            MACRO_ADDITEM(CUP_G_ESS_RGR_Facewrap_Skull,2);
+            MACRO_ADDITEM(CUP_H_PMC_Cap_Back_EP_Grey,2);
+            MACRO_ADDITEM(CUP_H_PMC_Cap_Grey,2);
+            MACRO_ADDITEM(CUP_muzzle_fh_MP5,4);
+            MACRO_ADDITEM(CUP_optic_1P87_RIS,2);
+            MACRO_ADDITEM(CUP_optic_MEPRO,2);
+            MACRO_ADDITEM(CUP_PMC_Facewrap_Skull,2);
+            MACRO_ADDITEM(CUP_U_CRYE_BLK_Roll,2);
+            MACRO_ADDITEM(G_Balaclava_Skull1,2);
+            MACRO_ADDITEM(G_Bandanna_aviator,2);
+            MACRO_ADDITEM(G_Bandanna_Skull1,2);
+            MACRO_ADDITEM(G_Bandanna_Skull2,2);
+            MACRO_ADDITEM(H_Beret_blk,2);
+            MACRO_ADDITEM(ItemCompass,10);
+            MACRO_ADDITEM(ItemMap,10);
+            MACRO_ADDITEM(optic_Aco_smg,2);
+            MACRO_ADDITEM(optic_Holosight_smg_blk_F,2);
+            MACRO_ADDITEM(optic_MRD_black,2);
+            MACRO_ADDITEM(optic_r1_high_lxWS,2);
+            MACRO_ADDITEM(tacgt_Light_TLR1,2);
+            MACRO_ADDITEM(tacgt_Surefire_Flashlight_Black,6);
+            MACRO_ADDITEM(tacs_Hat_Beret_BlackLogo,2);
+            MACRO_ADDITEM(tacs_milgp_battle_belt_assaulter_blk,2);
+            MACRO_ADDITEM(tacs_Uniform_Combat_RS_BS_BP_BB,2);
+            MACRO_ADDITEM(tacs_Uniform_Garment_LS_BS_BP_BB,2);
+            MACRO_ADDITEM(tacs_Uniform_Long_Plaid_Black,2);
+            MACRO_ADDITEM(U_O_R_Gorka_01_black_F,2);
+            MACRO_ADDITEM(V_Chestrig_blk,4);
+            MACRO_ADDITEM(V_MU_Chestrig_light_blk,2);
+            MACRO_ADDITEM(V_MU_HarnessO_blk,2);
+        };
         class TransportBackpacks {};
-        class TransportWeapons {};
+        class TransportWeapons {
+            MACRO_ADDWEAPON(ACE_Vector,4);
+            MACRO_ADDWEAPON(Binocular,4);
+            MACRO_ADDWEAPON(Camera_lxWS,4);
+            MACRO_ADDWEAPON(CUP_smg_MP5A5_Rail_VFG,2);
+            MACRO_ADDWEAPON(CUP_smg_vityaz_vfg_top_rail,2);
+            MACRO_ADDWEAPON(hgun_Glock19_auto_RF,2);
+            MACRO_ADDWEAPON(sgun_aa40_lxWS,2);
+            MACRO_ADDWEAPON(SMG_02_F,2);
+            MACRO_ADDWEAPON(SMG_05_F,2);
+        };
     };
     class CLASS(Supply_Special): CLASS(Supply_Keres) {
         displayName = "[TAC] Special";
