dc8f3e07cff4dfe7c834a23fc9e220d1225311b1
Mike-MF <29175040+Mike-MF@users.noreply.github.com>
2025-07-06 16:55:56 -0500

    Uniforms - Add Rolled Garments

--- addons/uniforms/config.cpp
+++ addons/uniforms/config.cpp
@@ -9,7 +9,12 @@ class CfgPatches {
             QCLASS(Uniform_PMC_Casual_Arcadian),
             QCLASS(Uniform_PMC_Casual_GTArmoury),
             QCLASS(Uniform_PMC_Casual_ACRE2),
-            QCLASS(Uniform_PMC_Casual_Theseus)
+            QCLASS(Uniform_PMC_Casual_Theseus),
+            QCLASS(Uniform_PMC_RolledGarment_1),
+            QCLASS(Uniform_PMC_RolledGarment_2),
+            QCLASS(Uniform_PMC_RolledGarment_3),
+            QCLASS(Uniform_PMC_RolledGarment_4),
+            QCLASS(Uniform_PMC_RolledGarment_5)
         };
         requiredVersion = REQUIRED_VERSION;
         requiredAddons[] = {"tacgt_main", "tacgt_theseus"};
