caec7c98547cfbf2323ceb3a244ace4d417d1f92
Mike-MF <29175040+Mike-MF@users.noreply.github.com>
2025-07-20 11:56:21 -0500

    Add Vests Component (#299)

---
+++ addons/vests/config.cpp
@@ -0,0 +1,21 @@
+#include "script_component.hpp"
+
+class CfgPatches {
+    class ADDON {
+        name = COMPONENT_NAME;
+        units[] = {};
+        weapons[] = {
+            QCLASS(EODHeavyVest_Blk),
+            QCLASS(EODHeavyVest_Gry)
+        };
+        requiredVersion = REQUIRED_VERSION;
+        requiredAddons[] = {"tacgt_main", "tacgt_theseus"};
+        skipWhenMissingDependencies = 1;
+        author = ECSTRING(main,Author);
+        authors[] = {"Mike"};
+        url = ECSTRING(main,URL);
+        VERSION_CONFIG;
+    };
+};
+
+#include "CfgWeapons.hpp"
