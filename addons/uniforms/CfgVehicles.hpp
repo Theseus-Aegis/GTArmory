3460eb42df44979d0fc3b6e33043cbec3b1034a4
Mike-MF <29175040+Mike-MF@users.noreply.github.com>
2025-07-20 11:58:03 -0500

    Uniforms - Add Rolled Garments (#298)

--- addons/uniforms/CfgVehicles.hpp
+++ addons/uniforms/CfgVehicles.hpp
@@ -1,5 +1,7 @@
 class CfgVehicles {
     class C_Uniform_ArtTShirt_01_v1_F;
+    class B_G_medic_F;
+    class Item_U_BG_Guerilla2_3;
     class Item_Base_F;
 
     // Server's Up YeeYees
@@ -87,5 +89,85 @@ class CfgVehicles {
         author = "GilleeDoo, Mike";
         uniformClass = QCLASS(Uniform_PMC_Casual_Theseus);
         hiddenSelectionsTextures[] = {QPATHTOF(data\pmc_casual_theseus_co.paa)};
+    };
+
+    class CLASS(Uniform_Garment_Rolled_1_V_Item): Item_U_BG_Guerilla2_3 {
+        displayName = "Garment Rolled (Green/OD)";
+        scope = 2;
+        scopeCurator = 2;
+        vehicleClass = "ItemsUniforms";
+        class TransportItems {
+            MACRO_ADDITEM(CLASS(Uniform_PMC_RolledGarment_1),1);
+        };
+    };
+    class CLASS(Uniform_Garment_Rolled_1_V): B_G_medic_F {
+        scope = 1;
+        author = "GilleeDoo, Mike";
+        uniformClass = QCLASS(Uniform_PMC_RolledGarment_1);
+        hiddenSelectionsTextures[] = {QPATHTOF(data\garment_rolled_1_co.paa)};
+    };
+
+    class CLASS(Uniform_Garment_Rolled_2_V_Item): Item_U_BG_Guerilla2_3 {
+        displayName = "Garment Rolled (Green Checkered/Tan)";
+        scope = 2;
+        scopeCurator = 2;
+        vehicleClass = "ItemsUniforms";
+        class TransportItems {
+            MACRO_ADDITEM(CLASS(Uniform_PMC_RolledGarment_2),1);
+        };
+    };
+    class CLASS(Uniform_Garment_Rolled_2_V): B_G_medic_F {
+        scope = 1;
+        author = "GilleeDoo, Mike";
+        uniformClass = QCLASS(Uniform_PMC_RolledGarment_2);
+        hiddenSelectionsTextures[] = {QPATHTOF(data\garment_rolled_2_co.paa)};
+    };
+
+    class CLASS(Uniform_Garment_Rolled_3_V_Item): Item_U_BG_Guerilla2_3 {
+        displayName = "Garment Rolled (Green/Coyote)";
+        scope = 2;
+        scopeCurator = 2;
+        vehicleClass = "ItemsUniforms";
+        class TransportItems {
+            MACRO_ADDITEM(CLASS(Uniform_PMC_RolledGarment_3),1);
+        };
+    };
+    class CLASS(Uniform_Garment_Rolled_3_V): B_G_medic_F {
+        scope = 1;
+        author = "GilleeDoo, Mike";
+        uniformClass = QCLASS(Uniform_PMC_RolledGarment_3);
+        hiddenSelectionsTextures[] = {QPATHTOF(data\garment_rolled_3_co.paa)};
+    };
+
+    class CLASS(Uniform_Garment_Rolled_4_V_Item): Item_U_BG_Guerilla2_3 {
+        displayName = "Garment Rolled (Red Checkered/Black)";
+        scope = 2;
+        scopeCurator = 2;
+        vehicleClass = "ItemsUniforms";
+        class TransportItems {
+            MACRO_ADDITEM(CLASS(Uniform_PMC_RolledGarment_4),1);
+        };
+    };
+    class CLASS(Uniform_Garment_Rolled_4_V): B_G_medic_F {
+        scope = 1;
+        author = "GilleeDoo, Mike";
+        uniformClass = QCLASS(Uniform_PMC_RolledGarment_4);
+        hiddenSelectionsTextures[] = {QPATHTOF(data\garment_rolled_4_co.paa)};
+    };
+
+    class CLASS(Uniform_Garment_Rolled_5_V_Item): Item_U_BG_Guerilla2_3 {
+        displayName = "Garment Rolled (Rust/Tan)";
+        scope = 2;
+        scopeCurator = 2;
+        vehicleClass = "ItemsUniforms";
+        class TransportItems {
+            MACRO_ADDITEM(CLASS(Uniform_PMC_RolledGarment_5),1);
+        };
+    };
+    class CLASS(Uniform_Garment_Rolled_5_V): B_G_medic_F {
+        scope = 1;
+        author = "GilleeDoo, Mike";
+        uniformClass = QCLASS(Uniform_PMC_RolledGarment_5);
+        hiddenSelectionsTextures[] = {QPATHTOF(data\garment_rolled_5_co.paa)};
     };
 };
