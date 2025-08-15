fe602c490a19d11ced80c13e4349ea8edea7116e
Patryk Kaniewski <p7tryk@gmail.com>
2025-08-15 23:16:38 +0200

    aegis: add aegis 5.45 RPK mags (#302)
    * aegis: add aegis 5.45 RPK mags
    
    * aegis: rename rpk mags to not confuse them with others
    
    * aegis: adjust rpk mags mass
    
    20 mass for 60rnd like cup one
    
    and i made up 16 as a number for 45rnd one since neither cup nor
    niarms nor aegis make sense so i just did 1.5x of 30rnd
    
    * aegis: rpk tracer typo
    
    * Update addons/ammunition/aegis/magazines/545x39.hpp
    
    ---------
    
    Co-authored-by: Mike-MF <29175040+Mike-MF@users.noreply.github.com>

--- addons/ammunition/aegis/CfgMagazines.hpp
+++ addons/ammunition/aegis/CfgMagazines.hpp
@@ -1,4 +1,6 @@
 class CfgMagazines {
+    class Aegis_60Rnd_545x39_Mag_F;
+    class Aegis_45Rnd_545x39_Mag_F;
     #include "magazines\127x33.hpp"
     #include "magazines\545x39.hpp"
     #include "magazines\57x28.hpp"
