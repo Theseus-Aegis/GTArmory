1d6f88862fc7daeb3172942937ca50c36c875bab
Mike-MF <29175040+Mike-MF@users.noreply.github.com>
2025-11-22 10:24:09 -0600

    Add Faction Rename Component (#308)

---
+++ addons/faction_rename/CfgFactionClasses.hpp
@@ -0,0 +1,69 @@
+class CfgFactionClasses {
+    // CUP Factions
+    class CUP_B_AFU {
+        displayName = "[CUP] Armed Forces of Ukraine";
+    };
+    class CUP_B_USMC {
+        displayName = "[CUP] USMC";
+    };
+    class CUP_B_US_Army {
+        displayName = "[CUP] United States Army";
+    };
+    class CUP_B_CDF {
+        displayName = "[CUP] Chernarus Defense Forces";
+    };
+    class CUP_O_RU {
+        displayName = "[CUP] Russian Federation";
+    };
+    class CUP_O_RUS_M {
+        displayName = "[CUP] Russian Federation (Modern)";
+    };
+    class CUP_O_ChDKZ {
+        displayName = "[CUP] Chernarussian Movement of the Red Star";
+    };
+    class CUP_I_NAPA {
+        displayName = "[CUP] National Party of Chernarus";
+    };
+    class CUP_B_RNZN {
+        displayName = "[CUP] New Zealand Navy";
+    };
+    class CUP_O_TK {
+        displayName = "[CUP] Takistani Army";
+    };
+    class CUP_O_TK_MILITIA {
+        displayName = "[CUP] Takistani Militia";
+    };
+    class CUP_B_US {
+        displayName = "[CUP] United States";
+    };
+    class CUP_B_CZ {
+        displayName = "[CUP] Army of the Czech Republic";
+    };
+    class CUP_B_GER {
+        displayName = "[CUP] Bundeswehr";
+    };
+    class CUP_I_TK_GUE {
+        displayName = "[CUP] Takistani Locals]";
+    };
+    class CUP_I_UN {
+        displayName = "[CUP] United Nations";
+    };
+    class CUP_O_SLA {
+        displayName = "[CUP] Sahrani Liberation Army";
+    };
+    class CUP_I_RACS {
+        displayName = "[CUP] Army Corps of Sahrani";
+    };
+    class CUP_B_GB {
+        displayName = "[CUP] British Armed Forces";
+    };
+    class CUP_I_PMC_ION {
+        displayName = "[CUP] ION PMC";
+    };
+    class CUP_B_HIL {
+        displayName = "[CUP] Horizon Islands Legion";
+    };
+    class CUP_Static_Ships {
+        displayName = "[CUP] Static Ships";
+    };
+};
---
+++ addons/faction_rename/script_component.hpp
@@ -0,0 +1,4 @@
+#define COMPONENT faction_rename
+#define COMPONENT_BEAUTIFIED Faction Rename
+#include "\x\tacgt\addons\main\script_mod.hpp"
+#include "\x\tacgt\addons\main\script_macros.hpp"
