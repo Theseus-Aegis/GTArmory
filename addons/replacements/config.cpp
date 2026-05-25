42fcada459e3df73a60296c72230e44f5221c5bb
Mike-MF <29175040+Mike-MF@users.noreply.github.com>
2026-05-25 13:27:28 -0500

    HEMTT 1.19.1 Suggestions (#313)

--- addons/replacements/config.cpp
+++ addons/replacements/config.cpp
@@ -4,7 +4,18 @@ class CfgPatches {
     class ADDON {
         name = COMPONENT_NAME;
         units[] = {QCLASS(Replacement_Box)};
-        weapons[] = {};
+        weapons[] = {
+            QCLASS(IFAK),
+            QCLASS(Grenade_Case),
+            QCLASS(Smoke_Grenade_Case),
+            QCLASS(M4_Weapon_Case),
+            QCLASS(Lootbox_GL),
+            QCLASS(Lootbox_Handgun),
+            QCLASS(Lootbox_LMG),
+            QCLASS(Lootbox_Marksman),
+            QCLASS(Lootbox_Rifle),
+            QCLASS(Lootbox_SMG)
+        };
         requiredVersion = REQUIRED_VERSION;
         requiredAddons[] = {
             "tacgt_main",
